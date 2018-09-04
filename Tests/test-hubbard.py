c2b4d94a2f96311a8a40e429038adfbd8fc05b5c
Sofia <sofiasw2@gmail.com>
2018-09-04 12:05:02 +0200

    Fixed spelling in Test/test-hubbard.py

--- Tests/test-hubbard.py
+++ Tests/test-hubbard.py
@@ -2,7 +2,7 @@ import Hubbard.hubbard as HH
 
 
 
-H = HH.Hubbard('../Examples/molecules/antracenes/2-antracene.XV')
+H = HH.Hubbard('../Examples/molecules/anthracenes/2-anthracene.XV')
 
 H.U = 5.
 H.random_density()
