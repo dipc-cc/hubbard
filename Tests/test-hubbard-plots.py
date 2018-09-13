71ae97065a505f2f56c171146209308e874b49fb
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-09-13 22:21:59 +0200

    test: split tests into plotting and quantitative checks

--- Tests/test-hubbard-plots.py
+++ Tests/test-hubbard-plots.py
@@ -18,31 +18,3 @@ H.plot_rs_wf(EnWindow=0.25, ispin=0, z=0.5)
 H.plot_rs_wf(EnWindow=0.25, ispin=1, z=0.5)
 H.plot_rs_wf(EnWindow=0.25, ispin=1, z=0.5, density=False)
 
-# Test also quantitatively that densities and eigenvalue spectrum are unchanged
-# Compute reference values:
-ev0, evec0 = H.Hup.eigh(eigvals_only=False)
-Etot0 = H.Etot*1
-
-# Reset density and iterate
-H.random_density()
-deltaN = 1.0
-
-while deltaN > 1e-10:
-    if deltaN > 0.1:
-        # preconditioning
-        deltaN, etot = H.iterate(mix=.1)
-    else:
-        deltaN, etot = H.iterate(mix=1)
-
-ev1, evec1 = H.Hup.eigh(eigvals_only=False)
-
-# Eigenvalues are easy to check
-if np.allclose(ev1, ev0):
-    print 'Eigenvalue check passed!'
-
-# Eigenvectors are a little more tricky due to arbitrary sign
-if np.allclose(np.abs(evec1), np.abs(evec0)):
-    print 'Eigenvector check passed!'
-
-# Total energy check:
-print Etot0, etot, Etot0-etot
---
+++ Tests/test-hubbard-quantitative.py
@@ -0,0 +1,41 @@
+import Hubbard.hubbard as HH
+import numpy as np
+
+# Test quantitatively that densities and eigenvalue spectrum are unchanged
+# using a reference molecule (already converged)
+H = HH.Hubbard('mol-ref/mol-ref.XV', U=3.5)
+
+# Determine reference values for the tests
+ev0, evec0 = H.Hup.eigh(eigvals_only=False)
+Etot0 = H.Etot*1
+
+# Reset density and iterate
+H.random_density()
+deltaN = 1.0
+i = 0
+while deltaN > 1e-10:
+    if deltaN > 0.1:
+        # preconditioning
+        deltaN, etot = H.iterate(mix=.1)
+    else:
+        deltaN, etot = H.iterate(mix=1)
+    i += 1
+    if i%10 == 0:
+        print i, deltaN, etot
+    
+ev1, evec1 = H.Hup.eigh(eigvals_only=False)
+
+# Eigenvalues are easy to check
+if np.allclose(ev1, ev0):
+    print 'Eigenvalue check passed'
+else:
+    raiseError('Eigenvalue check failed!!!')
+    
+# Eigenvectors are a little more tricky due to arbitrary sign
+if np.allclose(np.abs(evec1), np.abs(evec0)):
+    print 'Eigenvector check passed'
+else:
+    raiseError('Eigenvector check failed!!!')
+    
+# Total energy check:
+print Etot0, etot, Etot0-etot
