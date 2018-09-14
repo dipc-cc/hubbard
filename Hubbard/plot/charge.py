86a82b34b7e69703138dcdacabcfd8e371e1b8a9
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-09-14 22:43:01 +0200

    enh: added ChargeDifference() and SpinPolarization()

--- Hubbard/plot/charge.py
+++ Hubbard/plot/charge.py
@@ -5,7 +5,7 @@ from Hubbard.plot import GeometryPlot
 class Charge(GeometryPlot):
 
     def __init__(self, HubbardHamiltonian):
-        
+
         GeometryPlot.__init__(self, HubbardHamiltonian)
 
         # Compute total charge on each site
@@ -19,6 +19,55 @@ class Charge(GeometryPlot):
 
         # Write file
         fn = HubbardHamiltonian.get_label()+'-chg.pdf'
+        self.savefig(fn)
+
+        # Close plot
+        self.close()
+
+
+class ChargeDifference(GeometryPlot):
+
+    def __init__(self, HubbardHamiltonian):
+
+        GeometryPlot.__init__(self, HubbardHamiltonian)
+
+        # Compute charge difference to neutral atom on each site
+        chgdiff = HubbardHamiltonian.nup + HubbardHamiltonian.ndn
+        for ia in HubbardHamiltonian.pi_geom:
+            chgdiff[ia] -= HubbardHamiltonian.pi_geom.atoms[ia].Z-5
+
+        # Set values for the pi-network
+        self.ppi.set_array(chgdiff)
+
+        # Set colorbar limits
+        cmax = max(abs(chgdiff))
+        self.ppi.set_clim(-cmax, cmax)
+
+        # Write file
+        fn = HubbardHamiltonian.get_label()+'-chgdiff.pdf'
+        self.savefig(fn)
+
+        # Close plot
+        self.close()
+
+class SpinPolarization(GeometryPlot):
+
+    def __init__(self, HubbardHamiltonian):
+
+        GeometryPlot.__init__(self, HubbardHamiltonian)
+
+        # Compute charge difference to neutral atom on each site
+        spinpol = HubbardHamiltonian.nup - HubbardHamiltonian.ndn
+
+        # Set values for the pi-network
+        self.ppi.set_array(spinpol)
+
+        # Set colorbar limits
+        cmax = max(abs(spinpol))
+        self.ppi.set_clim(-cmax, cmax)
+
+        # Write file
+        fn = HubbardHamiltonian.get_label()+'-pol.pdf'
         self.savefig(fn)
 
         # Close plot
