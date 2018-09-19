8dd5fd8022a4c787e60b51702fccb384bbd3ca76
Sofia <sofiasw2@gmail.com>
2018-09-19 15:45:36 +0200

    enh: mark sp3 atom in all plots

--- Hubbard/hubbard.py
+++ Hubbard/hubbard.py
@@ -46,13 +46,13 @@ class Hubbard(object):
             print('Wrote '+self.fn+'.xyz')
         # Determine pz sites
         aux = []
-        sp3 = []
+        self.sp3 = []
         for ia in self.geom:
             if self.geom.atoms[ia].Z not in [5, 6, 7]:
                 aux.append(ia)
             idx = self.geom.close(ia,R=[1.2]) # C-H bond distance
             if self.geom.atoms[ia].Z == 6 and len(idx)==3:
-                sp3.append(ia)
+                self.sp3.append(ia)
         # Remove all sites not carbon-type
         self.pi_geom = self.geom.remove(aux)
         self.sites = len(self.pi_geom)
@@ -71,7 +71,7 @@ class Hubbard(object):
         print('Found %i B-atoms, %i C-atoms, %i N-atoms' %(nB, nC, nN))
         print(' ... B-atoms at sites', np.where(self.pi_geom.atoms.Z == 5)[0])
         print(' ... N-atoms at sites', np.where(self.pi_geom.atoms.Z == 7)[0])
-        print('Found  %i sp3 atoms'%(len(sp3)))
+        print('Found  %i sp3 atoms'%(len(self.sp3)))
         print('Neutral system corresponds to a total of %i electrons' %ntot)
         # Use default (low-spin) filling?
         if Ndn <= 0:
@@ -224,6 +224,8 @@ class Hubbard(object):
         axes.set_xlabel(r'$x$ (\AA)')
         axes.set_ylabel(r'$y$ (\AA)')
         axes.set_aspect('equal')
+        if len(self.sp3)>0:
+            axes.add_patch(patches.Circle((np.average(x[self.sp3]),np.average(y[self.sp3])),radius=1.4,alpha=0.15,fc='c'))
         return fig, axes, x, y, bdx
 
 
