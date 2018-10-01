b2c9857275caf042e4f75cfb789f2afd2a70fedc
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-01 23:39:47 +0200

    enh: added tests of the plot subpackage

--- Tests/test-hubbard-plots.py
+++ Tests/test-hubbard-plots.py
@@ -1,20 +1,37 @@
 import Hubbard.hubbard as HH
+import Hubbard.plot as plot
 import numpy as np
 
 # Test all plot functionalities of Hubbard module
 # using a reference molecule (already converged)
 
-H = HH.Hubbard('mol-ref/mol-ref.XV', U=3.5)
+H = HH.Hubbard('mol-ref/mol-ref.XV', U=3.5, what='xyz')
 
-# Produce plots
-H.plot_spectrum()
-H.plot_charge()
-H.plot_polarization()
-H.plot_rs_polarization()
-H.plot_wf(EnWindow=0.25, ispin=0)
-H.plot_wf(EnWindow=0.25, ispin=1)
-H.plot_wf(EnWindow=0.25, ispin=1, density=False)
-H.plot_rs_wf(EnWindow=0.25, ispin=0, z=0.5)
-H.plot_rs_wf(EnWindow=0.25, ispin=1, z=0.5)
-H.plot_rs_wf(EnWindow=0.25, ispin=1, z=0.5, density=False)
+# Old plotting routines
+if False:
+    H.plot_spectrum()
+    H.plot_charge()
+    H.plot_polarization()
+    H.plot_rs_polarization()
+    H.plot_wf(EnWindow=0.25, ispin=0)
+    H.plot_wf(EnWindow=0.25, ispin=1)
+    H.plot_wf(EnWindow=0.25, ispin=1, density=False)
+    H.plot_rs_wf(EnWindow=0.25, ispin=0, z=0.5)
+    H.plot_rs_wf(EnWindow=0.25, ispin=1, z=0.5)
+    H.plot_rs_wf(EnWindow=0.25, ispin=1, z=0.5, density=False)
 
+# new routines
+if True:
+    p = plot.Charge(H)
+    p.savefig('chg.pdf')
+
+    p = plot.ChargeDifference(H)
+    p.savefig('chgdiff.pdf')
+
+    p = plot.SpinPolarization(H)
+    p.annotate()
+    p.savefig('pol.pdf')
+
+    ev, evec = H.Hup.eigh(eigvals_only=False)
+    p = plot.Wavefunction(H, 500*evec[:, 10])
+    p.savefig('wf.pdf')
--- Tests/test-sisl.py
+++ Tests/test-sisl.py
@@ -7,7 +7,7 @@ graphene = sisl.geom.graphene()
 H = sisl.Hamiltonian(graphene)
 for ia, io in H:
     idx = H.geom.close(ia, R=[0.1, 1.43])
-    H[io,idx[0]] = 0.
-    H[io,idx[1]] = -2.7
+    H[io, idx[0]] = 0.
+    H[io, idx[1]] = -2.7
 
 print(H.eigh(k=[0., 0.5, 0.]))
