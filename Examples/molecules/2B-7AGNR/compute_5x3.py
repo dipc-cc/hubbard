4a51bd15b7a6480700d751955380649e8ae6a9d7
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-02 16:19:47 +0200

    examples: showcasing use of ncgroups to store different magnetic solutions

--- Examples/molecules/2B-7AGNR/compute_5x3.py
+++ Examples/molecules/2B-7AGNR/compute_5x3.py
@@ -3,20 +3,31 @@ import Hubbard.plot as plot
 import sys
 import numpy as np
 
-H = hh.HubbardHamiltonian('7AGNR2B_5x3.XV', t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz')
+fn = '7AGNR2B_5x3.XV'
+
+grp = 'AFM-AFM-AFM'
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
 H.polarize_sites([1, 99], dn=[80, 152])
 dn, etot = H.converge()
-H.save()
+H.save(grp)
 p = plot.SpinPolarization(H)
 p.annotate()
-p.savefig('pol_5x3.pdf')
+p.savefig('pol_5x3_%s.pdf'%grp)
 
-"""
-H.plot_wf(EnWindow=1.5, density=False)
-H.plot_wf(EnWindow=1.5, density=True)
-H.plot_rs_wf(EnWindow=1.5, density=False)
-H.plot_polarization()
-H.plot_charge()
-H.plot_rs_polarization()
-H.plot_spectrum(xmax=2.0, ymax=0.15)
-"""
+grp = 'AFM-FM-AFM'
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H.polarize_sites([1, 152], dn=[80, 99])
+dn, etot = H.converge()
+H.save(grp)
+p = plot.SpinPolarization(H)
+p.annotate()
+p.savefig('pol_5x3_%s.pdf'%grp)
+
+grp = 'FM-AFM-FM'
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H.polarize_sites([1, 80], dn=[99, 152])
+dn, etot = H.converge()
+H.save(grp)
+p = plot.SpinPolarization(H)
+p.annotate()
+p.savefig('pol_5x3_%s.pdf'%grp)
--- Examples/molecules/2B-7AGNR/compute_5x5.py
+++ Examples/molecules/2B-7AGNR/compute_5x5.py
@@ -3,20 +3,31 @@ import Hubbard.plot as plot
 import sys
 import numpy as np
 
-H = hh.HubbardHamiltonian('7AGNR2B_5x5.XV', t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz')
+fn = '7AGNR2B_5x5.XV'
+
+grp = 'AFM-AFM-AFM'
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
 H.polarize_sites([3, 99], dn=[82, 178])
 dn, etot = H.converge()
-H.save()
+H.save(grp)
 p = plot.SpinPolarization(H)
 p.annotate()
-p.savefig('pol_5x5.pdf')
+p.savefig('pol_5x5_%s.pdf'%grp)
 
-"""
-H.plot_wf(EnWindow=1.5, density=False)
-H.plot_wf(EnWindow=1.5, density=True)
-H.plot_rs_wf(EnWindow=1.5, density=False)
-H.plot_polarization()
-H.plot_charge()
-H.plot_rs_polarization()
-H.plot_spectrum(xmax=2.0, ymax=0.15)
-"""
+grp = 'AFM-FM-AFM'
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H.polarize_sites([3, 178], dn=[82, 99])
+dn, etot = H.converge()
+H.save(grp)
+p = plot.SpinPolarization(H)
+p.annotate()
+p.savefig('pol_5x5_%s.pdf'%grp)
+
+grp = 'FM-AFM-FM'
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H.polarize_sites([3, 82], dn=[99, 178])
+dn, etot = H.converge()
+H.save(grp)
+p = plot.SpinPolarization(H)
+p.annotate()
+p.savefig('pol_5x5_%s.pdf'%grp)
