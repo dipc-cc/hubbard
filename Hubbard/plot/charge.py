17c4dab42b257efdbf4297d21311db1c32bdd09e
Sofia <sofiasw2@gmail.com>
2018-10-03 15:24:25 +0200

    enh: added realspace function in ChargeDifference (charge.py)

--- Hubbard/plot/charge.py
+++ Hubbard/plot/charge.py
@@ -87,7 +87,7 @@ class ChargeDifference(GeometryPlot):
             charge[ia] -= HubbardHamiltonian.geom.atoms[ia].Z-5
 
         if 'realspace' in keywords:
-            self.__realspace__(charge, HubbardHamiltonian, **keywords)
+            self.__realspace__(HubbardHamiltonian, charge, **keywords)
         else:
             self.__orbitals__(charge, **keywords)
             
@@ -107,6 +107,28 @@ class ChargeDifference(GeometryPlot):
             if keywords['colorbar'] != False:
                 self.add_colorbar(self.ppi, label=r'$Q_\uparrow+Q_\downarrow-Q_\mathrm{NA}$ ($e$)')
 
+    def __realspace__(self, HubbardHamiltonian, charge, z=1.1, vmax=0.006, grid_unit=0.05, **keywords):
+        sc = sisl.SuperCell([self.xmax-self.xmin, self.ymax-self.ymin, 3.2])
+        grid = sisl.Grid(grid_unit, sc=sc)
+        
+        vecs = np.zeros((HubbardHamiltonian.sites, HubbardHamiltonian.sites))
+        vecs[0, :] = charge
+        H = HubbardHamiltonian.H.move([-self.xmin, -self.ymin, 0])
+        H.xyz[np.where(np.abs(H.xyz[:, 2]) < 1e-3), 2] = 0
+        H.set_sc(sc)
+        es = sisl.EigenstateElectron(vecs, np.zeros(HubbardHamiltonian.sites), H)
+        es.sub(0).psi(grid)
+        index = grid.index([0, 0, z])
+
+        # Plot only the real part
+        ax = self.axes.imshow(grid.grid[:, :, index[2]].T.real, cmap='seismic', origin='lower',
+                              vmax=vmax, vmin=-vmax, extent=self.extent)
+        # Colorbars
+        if 'colorbar' in keywords:
+            if keywords['colorbar'] != False:
+                # Charge density per unit of length in the z-direction
+                plt.colorbar(ax, label=r'$q_\uparrow+q_\downarrow-q_\mathrm{NA}$ ($e/$\AA)')
+        
 
 class SpinPolarization(GeometryPlot):
 
