61897cbb4839186fd4338b26ee3016acd9abf507
Sofia <sofiasw2@gmail.com>
2018-10-03 12:27:13 +0200

    added default keywords in SpinPolarization

--- Hubbard/plot/charge.py
+++ Hubbard/plot/charge.py
@@ -111,8 +111,14 @@ class SpinPolarization(GeometryPlot):
     def __init__(self, HubbardHamiltonian, **keywords):
 
         # Set default keywords
-        if 'cmap' not in keywords:
-            keywords['cmap'] = plt.cm.bwr
+        if 'realspace' in keywords:
+            if 'facecolor' not in keywords:
+                keywords['facecolor'] = 'None'
+            if 'cmap' not in keywords:
+                keywords['cmap'] = 'Greys'
+        else:
+            if 'cmap' not in keywords:
+                keywords['cmap'] = plt.cm.bwr
 
         GeometryPlot.__init__(self, HubbardHamiltonian, **keywords)
 
