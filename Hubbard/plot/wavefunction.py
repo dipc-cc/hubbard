3aaee5c5bbc053423dbef5ee3b0f7ffc12691075
Sofia <sofiasw2@gmail.com>
2018-10-03 17:56:17 +0200

    updated real space creating in wavefunction.py

--- Hubbard/plot/wavefunction.py
+++ Hubbard/plot/wavefunction.py
@@ -39,16 +39,8 @@ class Wavefunction(GeometryPlot):
         self.axes.scatter(x, y, -wf.real, 'g') # neg. part
     
     def __realspace__(self, HubbardHamiltonian, wf, z=1.1, vmax=0.006, grid_unit=0.05, **keywords):
-        # Set new sc to create real-space grid
-        sc = sisl.SuperCell([self.xmax-self.xmin, self.ymax-self.ymin, 3.2])
-        H = HubbardHamiltonian.H.move([-self.xmin, -self.ymin, 0])
-        H.xyz[np.where(np.abs(H.xyz[:, 2]) < 1e-3), 2] = 0
-        H.set_sc(sc)
-
-        # Create the real-space grid
-        grid = sisl.Grid(grid_unit, sc=H.sc, geometry=H)
-        sisl.electron.wavefunction(wf, grid, geometry=H)
-        index = grid.index([0, 0, z])
+        
+        grid, index = self.real_space_grid(wf, z, grid_unit) 
 
         # Create custom map to differenciate it from polarization cmap
         import matplotlib.colors as mcolors
