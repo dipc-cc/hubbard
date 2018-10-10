142c7e88aeaf34456de090831b6829d0cc02e425
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-10 21:36:02 +0200

    enh: only compute Zak-phase for bands within the plotting range

--- Examples/periodic/7AGNR/compute.py
+++ Examples/periodic/7AGNR/compute.py
@@ -14,16 +14,6 @@ else:
     H.converge()
 H.save()
 
-# Computation of band-resolved Zak-phases
-nx = 100
-zlist = []
-for band in range(H.Nup+7):
-    # compute Zak phase for bands near the gap
-    zak = H.get_Zak_phase(Nx=nx, sub=band)
-    zlist.append(zak)
-    z2 = int(np.round(np.abs(1-np.exp(1j*zak))/2, 0))
-    print 'Zak (Z2) for N=%i : %.4f (%i)'% (band, zak, z2)
-
 # Spin polarization plot
 p = plot.SpinPolarization(H, colorbar=True)
 p.set_title(r'$e_\mathrm{B}=%.2f$ eV, $U=%.2f$ eV'%(eB, U))
@@ -35,7 +25,9 @@ ev = H.eigh(k=[0, 0, 0])
 batoms = list(np.where(H.geom.atoms.Z == 5)[0])
 p = plot.Bandstructure(H, scale=2, ymax=2, projection=batoms)
 p.set_title(r'$e_\mathrm{B}=%.2f$ eV, $U=%.2f$ eV'%(eB, U))
-for i, zak in enumerate(zlist):
-    p.axes.annotate('%.2f'%zak, (0.13*(i%2), ev[i]-H.midgap), size=8)
+for i, evi in enumerate(ev):
+    if abs(evi-H.midgap) < 2.0:
+        zak = H.get_Zak_phase(Nx=100, sub=i)
+        p.axes.annotate('%.2f'%zak, (0.13*(i%2), evi-H.midgap), size=8)
 fo = fn.replace('.XV', '-zak-U%.2f-eB%.2f.pdf'%(U, eB))
 p.savefig('summary/'+fo)
