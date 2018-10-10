6cfc9a572e25b362701b825648b95c7ae7104b1b
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-10 21:02:33 +0200

    example: calculate Zak phase for 7AGNRs

--- Examples/periodic/7AGNR/README.md
+++ Examples/periodic/7AGNR/README.md
@@ -1,5 +1,4 @@
 In this folder we compute the electronic structure for 7AGNR-type ribbons,
 similar to that done by Carbonell-Sanroma et al, J. Phys. Chem. C 122, 16092−16099 (2018)
 
-    python compute.py 2B-7AGNR.XV <U-value>
-    
+    python compute.py 2B-7AGNR.XV <U-value> <Boron-onsite-energy>
--- Examples/periodic/7AGNR/compute.py
+++ Examples/periodic/7AGNR/compute.py
@@ -5,17 +5,37 @@ import sys
 
 fn = sys.argv[1]
 U = float(sys.argv[2])
+eB = float(sys.argv[3])
 
-H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=U, nsc=[3, 1, 1], kmesh=[51, 1, 1], what='xyz')
-H.converge()
+H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=U, eB=eB, nsc=[3, 1, 1], kmesh=[51, 1, 1], what='xyz')
+if U < 0.1:
+    H.converge(premix=1.0)
+else:
+    H.converge()
 H.save()
 
-p = plot.SpinPolarization(H, colorbar=True)
-p.annotate()
-p.savefig('polarization.pdf')
+# Computation of band-resolved Zak-phases
+nx = 100
+zlist = []
+for band in range(H.Nup+7):
+    # compute Zak phase for bands near the gap
+    zak = H.get_Zak_phase(Nx=nx, sub=band)
+    zlist.append(zak)
+    z2 = int(np.round(np.abs(1-np.exp(1j*zak))/2, 0))
+    print 'Zak (Z2) for N=%i : %.4f (%i)'% (band, zak, z2)
 
-# Project onto boron sites
+# Spin polarization plot
+p = plot.SpinPolarization(H, colorbar=True)
+p.set_title(r'$e_\mathrm{B}=%.2f$ eV, $U=%.2f$ eV'%(eB, U))
+fo = fn.replace('.XV', '-pol-U%.2f-eB%.2f.pdf'%(U, eB))
+p.savefig('summary/'+fo)
+
+# Bandstructure plot
+ev = H.eigh(k=[0, 0, 0])
 batoms = list(np.where(H.geom.atoms.Z == 5)[0])
-p = plot.Bandstructure(H, scale=2, projection=batoms)
-p.set_title(r'$U=%.2f$ eV: %s'%(U, fn.replace('_', '-')))
-p.savefig('bands_U%.2f.pdf'%U)
+p = plot.Bandstructure(H, scale=2, ymax=2, projection=batoms)
+p.set_title(r'$e_\mathrm{B}=%.2f$ eV, $U=%.2f$ eV'%(eB, U))
+for i, zak in enumerate(zlist):
+    p.axes.annotate('%.2f'%zak, (0.13*(i%2), ev[i]-H.midgap), size=8)
+fo = fn.replace('.XV', '-zak-U%.2f-eB%.2f.pdf'%(U, eB))
+p.savefig('summary/'+fo)
