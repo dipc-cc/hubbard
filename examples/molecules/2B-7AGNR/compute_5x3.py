bc0ebffa6df617629ae26e3edc83abd5138b9a11
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-19 17:25:58 +0200

    examples: added call to LDOSmaps

--- examples/molecules/2B-7AGNR/compute_5x3.py
+++ examples/molecules/2B-7AGNR/compute_5x3.py
@@ -3,20 +3,26 @@ import Hubbard.plot as plot
 import sys
 import numpy as np
 
+pol = False
 fn = '7AGNR2B_5x3.XV'
 
 grp = 'AFM-AFM-AFM'
 H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
-H.set_polarization([1, 99], dn=[80, 152])
+if pol:
+    H.set_polarization([1, 99], dn=[80, 152])
 dn, etot = H.converge()
 H.save(grp)
 p = plot.SpinPolarization(H)
 p.annotate()
 p.savefig('pol_5x3_%s.pdf'%grp)
 
+p = plot.LDOSmap(H)
+p.savefig('pol_5x3_%s_ldos.pdf'%grp)
+
 grp = 'AFM-FM-AFM'
 H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
-H.set_polarization([1, 152], dn=[80, 99])
+if pol:
+    H.set_polarization([1, 152], dn=[80, 99])
 dn, etot = H.converge()
 H.save(grp)
 p = plot.SpinPolarization(H)
@@ -25,7 +31,8 @@ p.savefig('pol_5x3_%s.pdf'%grp)
 
 grp = 'FM-AFM-FM'
 H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
-H.set_polarization([1, 80], dn=[99, 152])
+if pol:
+    H.set_polarization([1, 80], dn=[99, 152])
 dn, etot = H.converge()
 H.save(grp)
 p = plot.SpinPolarization(H)
--- examples/molecules/2B-7AGNR/compute_5x5.py
+++ examples/molecules/2B-7AGNR/compute_5x5.py
@@ -3,20 +3,26 @@ import Hubbard.plot as plot
 import sys
 import numpy as np
 
+pol = False
 fn = '7AGNR2B_5x5.XV'
 
 grp = 'AFM-AFM-AFM'
 H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
-H.set_polarization([3, 99], dn=[82, 178])
+if pol:
+    H.set_polarization([3, 99], dn=[82, 178])
 dn, etot = H.converge()
 H.save(grp)
 p = plot.SpinPolarization(H)
 p.annotate()
 p.savefig('pol_5x5_%s.pdf'%grp)
 
+p = plot.LDOSmap(H)
+p.savefig('pol_5x5_%s_ldos.pdf'%grp)
+
 grp = 'AFM-FM-AFM'
 H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
-H.set_polarization([3, 178], dn=[82, 99])
+if pol:
+    H.set_polarization([3, 178], dn=[82, 99])
 dn, etot = H.converge()
 H.save(grp)
 p = plot.SpinPolarization(H)
@@ -25,7 +31,8 @@ p.savefig('pol_5x5_%s.pdf'%grp)
 
 grp = 'FM-AFM-FM'
 H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
-H.set_polarization([3, 82], dn=[99, 178])
+if pol:
+    H.set_polarization([3, 82], dn=[99, 178])
 dn, etot = H.converge()
 H.save(grp)
 p = plot.SpinPolarization(H)
