1ae188bb5cb55f86d210be9d6474eb722b57c142
Sofia <sofiasw2@gmail.com>
2018-10-29 20:29:57 +0100

    tests: updated test-hubbard-quick.py

--- tests/test-hubbard-quick.py
+++ tests/test-hubbard-quick.py
@@ -1,13 +1,18 @@
 from __future__ import print_function
 
 import Hubbard.hamiltonian as hh
+import sisl
+
+# Build sisl Geometry object
+fn = sisl.get_sile('mol-ref/mol-ref.XV').read_geom()
+fn.sc.set_nsc([1,1,1])
 
 # Run one iteration
-H = hh.HubbardHamiltonian('mol-ref/mol-ref.XV', U=5.0)
+H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=5.0)
 dn, etot = H.iterate(mix=.1)
 print(dn, etot)
 
 # Run also one iteration with data from ncfile
-H = hh.HubbardHamiltonian('mol-ref/mol-ref.XV', U=3.5)
+H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=3.5)
 dn, etot = H.iterate(mix=1)
 print(dn, etot)
