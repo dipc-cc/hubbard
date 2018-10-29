259eb0ca8a1410f561cfbc7a2fccac46fbda6773
Sofia <sofiasw2@gmail.com>
2018-10-29 19:16:30 +0100

    examples: updated 2B-7AGNR/compute_5x5.py creating sisl geometry object before starting HubbardHamiltonian

--- examples/molecules/2B-7AGNR/compute_5x5.py
+++ examples/molecules/2B-7AGNR/compute_5x5.py
@@ -2,12 +2,17 @@ import Hubbard.hamiltonian as hh
 import Hubbard.plot as plot
 import sys
 import numpy as np
+import sisl
 
 pol = False
-fn = '7AGNR2B_5x5.XV'
+
+# Build sisl Geometry object
+fn = sisl.get_sile('7AGNR2B_5x5.XV').read_geom()
+fn.sc.set_nsc([1,1,1])
+fn = fn.move(-fn.center(what='xyz'))
 
 grp = 'AFM-AFM-AFM'
-H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H = hh.HubbardHamiltonian(fn, fn_title='7AGNR2B_5x5', t1=2.7, t2=0.2, t3=.18, U=5.0, ncgroup=grp)
 if pol:
     H.set_polarization([3, 99], dn=[82, 178])
 dn, etot = H.converge()
@@ -20,7 +25,7 @@ p = plot.LDOSmap(H)
 p.savefig('pol_5x5_%s_ldos.pdf'%grp)
 
 grp = 'AFM-FM-AFM'
-H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H = hh.HubbardHamiltonian(fn, fn_title='7AGNR2B_5x5', t1=2.7, t2=0.2, t3=.18, U=5.0, ncgroup=grp)
 if pol:
     H.set_polarization([3, 178], dn=[82, 99])
 dn, etot = H.converge()
@@ -30,7 +35,7 @@ p.annotate()
 p.savefig('pol_5x5_%s.pdf'%grp)
 
 grp = 'FM-AFM-FM'
-H = hh.HubbardHamiltonian(fn, t1=2.7, t2=0.2, t3=.18, U=5.0, what='xyz', ncgroup=grp)
+H = hh.HubbardHamiltonian(fn, fn_title='7AGNR2B_5x5', t1=2.7, t2=0.2, t3=.18, U=5.0, ncgroup=grp)
 if pol:
     H.set_polarization([3, 82], dn=[99, 178])
 dn, etot = H.converge()
