5eaf5e9de61650a9c6ee77352660cf550aee5a2b
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2018-10-31 21:23:26 +0100

    enh: fixed direction of intercell bonds

--- Hubbard/plot/bonds.py
+++ Hubbard/plot/bonds.py
@@ -34,12 +34,11 @@ class BondOrder(GeometryPlot):
                 plt.plot(x, y, c='k', ls='-', lw=4, solid_capstyle='round')
             else:
                 # intercell bond
-                R /= np.dot(R, R)**.5
-                R *= d
-                x = [xr[0], xr[0]+R[0]/2]
-                y = [xr[1], xr[1]+R[1]/2]
-                plt.plot(x, y, c='k', ls='-', lw=4, solid_capstyle='round')
-                x = [xc[0], xc[0]-R[0]/2]
-                y = [xc[1], xc[1]-R[1]/2]
+                cell = H.geom.sc.cell
+                P = np.dot(cell, R)
+                j = np.argmax(np.abs(P))
+                R -= np.sign(P[j])*cell[j] # Subtract relevant lattice vector
+                x = [xr[0], xr[0]-R[0]/2]
+                y = [xr[1], xr[1]-R[1]/2]
                 plt.plot(x, y, c='k', ls='-', lw=4, solid_capstyle='round')
             self.axes.text(sum(x)/2, sum(y)/2, r'%.3f'%BO[r, c], ha="center", va="center", rotation=15, size=6, bbox=bbox_props)
