18218c2cc2d64157f08a9c69393eafb61240cbee
Sofia <sofiasw2@gmail.com>
2018-12-18 10:33:03 +0100

    test: some cleaning in test-hubbard-plots.py

--- tests/test-hubbard-plots.py
+++ tests/test-hubbard-plots.py
@@ -13,38 +13,35 @@ fn = fn.move(-fn.center(what='xyz')).rotate(220, [0,0,1])
 
 H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=3.5)
 
-# new routines
-if True:
-    p = plot.Charge(H, colorbar=True)
-    p.savefig('chg.pdf')
+p = plot.Charge(H, colorbar=True)
+p.savefig('chg.pdf')
 
-    p = plot.Charge(H, realspace=True, colorbar=True)
-    p.savefig('chg_rs.pdf')
+p = plot.Charge(H, realspace=True, colorbar=True)
+p.savefig('chg_rs.pdf')
 
-    p = plot.ChargeDifference(H)
-    p.savefig('chgdiff.pdf')
+p = plot.ChargeDifference(H)
+p.savefig('chgdiff.pdf')
 
-    p = plot.ChargeDifference(H, realspace=True)
-    p.savefig('chgdiff_rs.pdf')
+p = plot.ChargeDifference(H, realspace=True)
+p.savefig('chgdiff_rs.pdf')
 
-    p = plot.SpinPolarization(H)
-    p.annotate()
-    p.savefig('pol.pdf')
+p = plot.SpinPolarization(H)
+p.annotate()
+p.savefig('pol.pdf')
 
-    p = plot.SpinPolarization(H, realspace=True)
-    p.annotate()
-    p.savefig('pol_rs.pdf')
+p = plot.SpinPolarization(H, realspace=True)
+p.savefig('pol_rs.pdf')
 
-    ev, evec = H.eigh(eigvals_only=False, spin=0)
-    p = plot.Wavefunction(H, 500*evec[:, 10])
-    p.savefig('wf.pdf')
+ev, evec = H.eigh(eigvals_only=False, spin=0)
+p = plot.Wavefunction(H, 500*evec[:, 10])
+p.savefig('wf.pdf')
 
-    ev, evec = H.eigh(eigvals_only=False, spin=0)
-    p = plot.Wavefunction(H, 10*evec[:, 10], realspace=True)
-    p.savefig('wf_rs.pdf')
+ev, evec = H.eigh(eigvals_only=False, spin=0)
+p = plot.Wavefunction(H, 10*evec[:, 10], realspace=True)
+p.savefig('wf_rs.pdf')
 
-    p = plot.Spectrum(H)
-    p.savefig('spectrum.pdf')
+p = plot.Spectrum(H)
+p.savefig('spectrum.pdf')
 
-    p = plot.LDOSmap(H)
-    p.savefig('ldos.pdf')
+p = plot.LDOSmap(H)
+p.savefig('ldos.pdf')
