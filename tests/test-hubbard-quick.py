9afe543bc2af35e03252b5c8d8d2e6c96eab07d2
Sofia <sofiasw2@gmail.com>
2019-01-17 11:36:48 +0100

    test: updated test-hubbard-quick

--- tests/test-hubbard-quick.py
+++ tests/test-hubbard-quick.py
@@ -9,12 +9,14 @@ fn.sc.set_nsc([1,1,1])
 
 # Run one iteration
 H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=5.0)
-dn, etot = H.iterate(mix=.1)
-print(dn, etot)
+dn = H.iterate(mix=.1)
+print(dn, H.Etot)
 
 # Run also one iteration with data from ncfile
 H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=3.5)
-dn, etot = H.iterate(mix=1)
+dn = H.iterate(mix=1)
+etot = 1*H.Etot
 
-for d, e in [H.iterate2(mix=1), H.iterate3(mix=1)]:
+for d in [H.iterate2(mix=1), H.iterate3(mix=1)]:
+    e = H.Etot
     print(d-dn, e-etot)
