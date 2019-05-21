2e00cc994f6a97bd75d594c0f2112928a0408562
Sofia Sanz <sofiasw2@gmail.com>
2019-05-21 17:31:15 +0200

    tests: updated test-hubbard-quick script

--- tests/test-hubbard-quick.py
+++ tests/test-hubbard-quick.py
@@ -4,18 +4,22 @@ import Hubbard.hamiltonian as hh
 import sisl
 
 # Build sisl Geometry object
-fn = sisl.get_sile('mol-ref/mol-ref.XV').read_geometry()
-fn.sc.set_nsc([1,1,1])
+molecule = sisl.get_sile('mol-ref/mol-ref.XV').read_geometry()
+molecule.sc.set_nsc([1,1,1])
 
 # Run one iteration
-H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=5.0)
+H = hh.HubbardHamiltonian(molecule, U=5.0)
+H.random_density()
 dn = H.iterate(mix=.1)
-print(dn, H.Etot)
+H.save('mol-ref/test.nc')
+print(dn, H.etot)
 
 # Run also one iteration with data from ncfile
-H = hh.HubbardHamiltonian(fn, fn_title='mol-ref/mol-ref', U=3.5)
+H = hh.HubbardHamiltonian(molecule, U=3.5)
+H.read('mol-ref/mol-ref.nc')
 dn = H.iterate(mix=1)
 etot = 1*H.Etot
+print(dn, etot)
 
 for d in [H.iterate2(mix=1), H.iterate3(mix=1)]:
     e = H.Etot
