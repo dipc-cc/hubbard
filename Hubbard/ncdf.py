53cbfa49bee5c44e81356ca997ef41c34ab764eb
Sofia Sanz <sofiasw2@gmail.com>
2019-05-27 16:47:36 +0200

    enh: created class to read/write HubbardHamiltonian data

---
+++ Hubbard/ncdf.py
@@ -0,0 +1,96 @@
+"""
+
+:mod:`Hubbard.ncdf`
+==========================
+
+Function for the meanfield Hubbard Hamiltonian
+
+.. currentmodule:: Hubbard.ncdf
+
+"""
+
+from __future__ import print_function
+import numpy as np
+import netCDF4 as NC
+import sisl
+import hashlib
+
+class read(object):
+    
+    def __init__(self, fn, ncgroup='default'):
+        print('Reading', fn)
+        ncf = NC.Dataset(fn, 'r')
+        self.ncgroups = ncf.groups
+        self.U = ncf[ncgroup]['U'][0]
+        self.nup = ncf[ncgroup]['Density'][0][0]
+        self.ndn = ncf[ncgroup]['Density'][0][1]
+        self.Etot = ncf[ncgroup]['Etot'][0]
+        self.Nup = ncf[ncgroup]['Nup'][0]
+        self.Ndn = ncf[ncgroup]['Ndn'][0]
+        ncf.close()
+
+
+class write(object):
+
+    def __init__(self, HubbardHamiltonian, fn, ncgroup='default'):
+        H = HubbardHamiltonian
+        try:
+            ncf = NC.Dataset(fn, 'a')
+            print('Appending to', fn)
+        except:
+            print('Initiating', fn)
+            ncf = NC.Dataset(fn, 'w')
+        g = self.init_ncgrp(H, ncf, ncgroup)
+        myhash, s = self.gethash(H)
+        i = np.where(ncf[ncgroup]['hash'][:] == myhash)[0]
+        if len(i) == 0:
+            i = len(ncf[ncgroup]['hash'][:])
+            if g:
+                print('Warning! Attempting to save into file that contains different calculation in that ncgroup')
+                # Create new ncgroup to avoid overlap between different cacluations
+                ncgroup = 'new_group'
+                self.init_ncgrp(H, ncf, ncgroup)
+        else:
+            i = i[0]
+        ncf[ncgroup]['hash'][i] = myhash
+        ncf[ncgroup]['U'][i] = H.U
+        ncf[ncgroup]['Nup'][i] = H.Nup
+        ncf[ncgroup]['Ndn'][i] = H.Ndn
+        ncf[ncgroup]['Density'][i, 0] = H.nup
+        ncf[ncgroup]['Density'][i, 1] = H.ndn
+        ncf[ncgroup]['Etot'][i] = H.Etot
+        ncf.sync()
+        print('Wrote (U,Nup,Ndn)=(%.2f,%i,%i) data to %s{%s}'%(H.U, H.Nup, H.Ndn, fn, ncgroup))
+
+        
+    def init_ncgrp(self, HubbardHamiltonian, ncf, ncgroup):
+        H = HubbardHamiltonian
+        if ncgroup not in ncf.groups:
+            # create group
+            ncf.createGroup(ncgroup)
+            ncf[ncgroup].createDimension('unl', None)
+            ncf[ncgroup].createDimension('spin', 2)
+            ncf[ncgroup].createDimension('sites', len(H.geom))
+            ncf[ncgroup].createVariable('hash', 'i8', ('unl',))
+            ncf[ncgroup].createVariable('U', 'f8', ('unl',))
+            ncf[ncgroup].createVariable('Nup', 'i4', ('unl',))
+            ncf[ncgroup].createVariable('Ndn', 'i4', ('unl',))
+            ncf[ncgroup].createVariable('Density', 'f8', ('unl', 'spin', 'sites'))
+            ncf[ncgroup].createVariable('Etot', 'f8', ('unl',))
+            ncf.sync()
+        else:
+            return True
+
+    def gethash(self, HubbardHamiltonian):
+        H = HubbardHamiltonian
+        s = ''
+        s += 't1=%.2f '%H.t1
+        s += 't2=%.2f '%H.t2
+        s += 't3=%.2f '%H.t3
+        s += 'U=%.2f '%H.U
+        s += 'eB=%.2f '%H.eB
+        s += 'eN=%.2f '%H.eN
+        s += 'Nup=%.2f '%H.Nup
+        s += 'Ndn=%.2f '%H.Ndn
+        myhash = int(hashlib.md5(s.encode('utf-8')).hexdigest()[:7], 16)
+        return myhash, s
