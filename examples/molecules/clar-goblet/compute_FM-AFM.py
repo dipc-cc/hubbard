9ed9f90443604964469c50acbb456027efd1e049
Sofia Sanz <sofiasw2@gmail.com>
2019-05-28 16:07:23 +0200

    examples: updated clar-goblet/compute_FM-AFM.py

--- examples/molecules/clar-goblet/compute_FM-AFM.py
+++ examples/molecules/clar-goblet/compute_FM-AFM.py
@@ -1,36 +1,47 @@
 import Hubbard.hamiltonian as hh
 import Hubbard.plot as plot
 import sys
+import Hubbard.ncdf as ncdf
 import numpy as np
 import sisl
 
 # Build sisl Geometry object
-fn = sisl.get_sile('clar-goblet.xyz').read_geometry()
-fn.sc.set_nsc([1,1,1])
-fn = fn.move(-fn.center(what='xyz'))
+mol = sisl.get_sile('clar-goblet.xyz').read_geometry()
+mol.sc.set_nsc([1,1,1])
+mol = mol.move(-mol.center(what='xyz'))
 
 # 3NN tight-binding model
-H = hh.HubbardHamiltonian(fn, fn_title='clar-goblet', t1=2.7, t2=.2, t3=.18)
+H = hh.HubbardHamiltonian(mol, t1=2.7, t2=.2, t3=.18)
 
 f = open('FM-AFM.dat', 'w')
 
 for u in np.linspace(0.0, 3.5, 15):
     # We approach the solutions for different U values
     H.U = u
-    H.read() # Try reading, if we already have density on file
-
+    try:
+        c = ncdf.read('clar-goblet.nc', ncgroup='AFM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
+        # Use // c.hash == ncdf.gethash(H).hash // To ensure that it is following the same calculation  
+        H.nup, H.ndn = c.nup, c.ndn
+    except:
+        H.random_density()
+    
     # AFM case first
-    dn = H.converge(mix=.5, tol=1e-7)
+    dn = H.converge(tol=1e-10)
     eAFM = H.Etot
-    H.save() # Computed density to file
+    ncdf.write(H, 'clar-goblet.nc', ncgroup='AFM_U%i'%(H.U*1000))
 
     # Now FM case
     H.Nup += 1 # change to two more up-electrons than down
     H.Ndn -= 1
-    H.read()
-    dn = H.converge(mix=.01, tol=1e-4)
+    try:
+        c = ncdf.read('clar-goblet.nc', ncgroup='FM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
+        # Use // c.hash == ncdf.gethash(H).hash // To ensure that it is following the same calculation 
+        H.nup, H.ndn = c.nup, c.ndn
+    except:
+        H.random_density()
+    dn = H.converge(tol=1e-10)
     eFM = H.Etot
-    H.save()
+    ncdf.write(H, 'clar-goblet.nc', ncgroup='FM_U%i'%(H.U*1000))
 
     # Revert the imbalance for next loop
     H.Nup -= 1
