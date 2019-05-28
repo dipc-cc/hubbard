048ba35d3402fb2762b0582e4580b38493e7879e
Sofia Sanz <sofiasw2@gmail.com>
2019-05-28 17:03:17 +0200

    examples: updated kondo-paper/fig_S14.py

--- examples/molecules/kondo-paper/fig_S14.py
+++ examples/molecules/kondo-paper/fig_S14.py
@@ -1,14 +1,15 @@
 import Hubbard.hamiltonian as hh
+import Hubbard.ncdf as ncdf
 import sys
 import numpy as np
 import sisl
 
 # Build sisl Geometry object
-fn = sisl.get_sile('junction-2-2.XV').read_geometry()
-fn.sc.set_nsc([1,1,1])
+mol = sisl.get_sile('junction-2-2.XV').read_geometry()
+mol.sc.set_nsc([1,1,1])
 
 # 3NN tight-binding model
-H = hh.HubbardHamiltonian(fn, fn_title='junction-2-2', t1=2.7, t2=0.2, t3=.18)
+H = hh.HubbardHamiltonian(mol, t1=2.7, t2=0.2, t3=.18)
 
 # Output file to collect the energy difference between
 # FM and AFM solutions
@@ -17,20 +18,31 @@ f = open('FM-AFM.dat', 'w')
 for u in np.linspace(0.0, 1.4, 15):
     # We approach the solutions from above, starting at U=4eV
     H.U = 4.4-u
-    H.read() # Try reading, if we already have density on file
-
     # AFM case first
+    try:
+        c = ncdf.read('fig_S14.nc', ncgroup='AFM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
+        # Use // c.hash == ncdf.gethash(H).hash // To ensure that it is following the same calculation  
+        H.nup, H.ndn = c.nup, c.ndn
+    except:
+        H.random_density()
+
     dn = H.converge()
     eAFM = H.Etot
-    H.save() # Computed density to file
+    ncdf.write(H, 'fig_S14.nc', ncgroup='AFM_U%i'%(H.U*1000))
 
     # Now FM case
     H.Nup += 1 # change to two more up-electrons than down
     H.Ndn -= 1
-    H.read()
+    try:
+        c = ncdf.read('fig_S14.nc', ncgroup='FM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
+        # Use // c.hash == ncdf.gethash(H).hash // To ensure that it is following the same calculation  
+        H.nup, H.ndn = c.nup, c.ndn
+    except:
+        H.random_density()
+
     dn = H.converge()
     eFM = H.Etot
-    H.save()
+    ncdf.write(H, 'fig_S14.nc', ncgroup='FM_U%i'%(H.U*1000))
 
     # Revert the imbalance for next loop
     H.Nup -= 1
