25e32aa2fda332957efcbfe1a4ba79ba3c1b0ebc
Sofia Sanz <sofiasw2@gmail.com>
2019-05-28 18:49:53 +0200

    examples: updated fig_S11-S13.py

--- examples/molecules/kondo-paper/fig_S11-S13.py
+++ examples/molecules/kondo-paper/fig_S11-S13.py
@@ -2,35 +2,39 @@ import Hubbard.hamiltonian as hh
 import Hubbard.plot as plot
 import sys
 import numpy as np
+import Hubbard.ncdf as ncdf
 import sisl
 
 # Build sisl Geometry object
-fn = sisl.get_sile('junction-2-2.XV').read_geometry()
-fn.sc.set_nsc([1,1,1])
-fn = fn.move(-fn.center(what='xyz')).rotate(220, [0,0,1])
+mol = sisl.get_sile('junction-2-2.XV').read_geometry()
+mol.sc.set_nsc([1,1,1])
+mol = mol.move(-mol.center(what='xyz')).rotate(220, [0,0,1])
 
 # 3NN tight-binding model
-H = hh.HubbardHamiltonian(fn, fn_title='junction-2-2', t1=2.7, t2=0.2, t3=.18)
+H = hh.HubbardHamiltonian(mol, t1=2.7, t2=0.2, t3=.18)
 
 for u in [0.0, 3.5]:
-    # We approach the solutions from above, starting at U=4eV
     H.U = u
-    H.read() # Try reading, if we already have density on file
-    H.converge()
-    H.save() # Computed density to file
+    if H.U == 0:
+        lab = 'Fig_S12'
+    else:
+        lab = 'Fig_S13'
+        try:
+            c = ncdf.read('fig_S11-S13.nc') # Try reading, if we already have density on file
+            H.nup, H.ndn = c.nup, c.ndn
+        except:
+            H.random_density()
+        H.converge()
+        ncdf.write(H, 'fig_S11-S13.nc')
 
     # Plot Eigenspectrum
     p = plot.Spectrum(H, ymax=0.12)
     p.set_title(r'3NN, $U=%.2f$ eV'%H.U)
     p.savefig('Fig_S11_eigenspectrum_U%i.pdf'%(H.U*100))
 
-    # Plot HOMO and LUMO level wavefunctions for up- and down-electrons for U=3.5 eV
+    # Plot HOMO and LUMO level wavefunctions for up- and down-electrons
     spin = ['up', 'dn']
     N = [H.Nup, H.Ndn]
-    if H.U == 0:
-        lab = 'Fig_S12'
-    else:
-        lab = 'Fig_S13'
     for i in range(2):
         ev, evec = H.eigh(eigvals_only=False, spin=i)
         ev -= H.midgap
