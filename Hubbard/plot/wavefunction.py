6eed5d38a61039adf68c3156ab30be1e1cce43f2
Sofia <sofiasw2@gmail.com>
2019-06-05 16:05:51 +0200

    enh: plor wavefunction with complex phase

--- Hubbard/plot/wavefunction.py
+++ Hubbard/plot/wavefunction.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class Wavefunction(GeometryPlot):
 
-    def __init__(self, HubbardHamiltonian, wf, **keywords):
+    def __init__(self, HubbardHamiltonian, wf, label=r'Phase', **keywords):
 
         # Set default keywords
         if 'realspace' in keywords:
@@ -36,6 +36,19 @@ class Wavefunction(GeometryPlot):
 
             assert len(x) == len(wf)
 
-            self.axes.scatter(x, y, wf.real, 'r') # pos. part, marker AREA is proportional to data
-            self.axes.scatter(x, y, -wf.real, 'g') # neg. part
+            fun, phase = np.absolute(wf), np.angle(wf)
+            # Make phases -pi -> pi
+            phase[np.where(phase+np.pi < 1e-5)] = np.pi 
+
+            import matplotlib.colors as mcolors
+            custom_map = mcolors.LinearSegmentedColormap.from_list(name='custom_map', colors =['b', 'red'], N=100)
+            ax = self.axes.scatter(x, y, s=fun, c=phase, cmap=custom_map)
+
+            # Colorbars
+            if 'colorbar' in keywords:
+                if keywords['colorbar'] != False:
+                    cb = self.fig.colorbar(ax, label=label)
+                    if 'ticks' in keywords:
+                        cb.set_ticks([np.pi, 3*np.pi/4, np.pi/2, np.pi/4, 0, -np.pi/4., -np.pi/2., -3*np.pi/4, -np.pi,])
+                        cb.set_ticklabels([r'$\pi$', r'$3\pi/4$', r'$\pi/2$', r'$\pi/4$', r'$0$', r'-$\pi/4$', r'-$\pi/2$', r'-$3\pi/4$', r'-$\pi$'])
 
