721feb726b642e1cdb29c4e24ee6a9dbb6067741
Sofia <sofiasw2@gmail.com>
2019-06-13 17:20:37 +0200

    examples: compute functions from chiral-geom in compute.py

---
+++ examples/periodic/chiralGNRs/compute.py
@@ -0,0 +1,47 @@
+import sisl
+import Hubbard.hamiltonian as hh
+import Hubbard.plot as plot
+import numpy as np
+import os
+ch = __import__('chiral-geom')
+op = __import__('open_boundary')
+
+n = 3
+m = [1,2]
+w = [4,6,8]
+
+for m_i in m:
+    for w_i in w:
+        geom = ch.cgnr(n,m_i,w_i)
+        directory = '%i-%i-%i'%(n,m_i,w_i)
+        print('Doing', directory)
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+        ch.analyze_edge(geom)
+        ch.analyze(geom)
+        ch.plot_states(geom)
+        ch.gap_exp(geom)
+
+        h = sisl.Hamiltonian(geom)
+        for ia in geom:
+            idx = geom.close(ia, R=[0., 1.43])
+            h[ia, idx[0]] = 0
+            h[ia, idx[1]] = -2.7
+        # Plot surface and bulk density of states
+        op.open_boundary(h, directory, xlim=0.5)
+
+if False:
+    # Plot bulk and surface DOS of 1D chain to test the funcion
+    directory='./'
+    print('Doing', directory)
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+    geom = sisl.Geometry([[0,0,0]], atom='C', sc=[1.42,10,10])
+    geom.set_nsc([3,1,1])
+    h = sisl.Hamiltonian(geom)
+    for ia in geom:
+        idx = geom.close(ia, R=[0., 1.43])
+        h[ia, idx[0]] = 0
+        h[ia, idx[1]] = -2.7
+    op.open_boundary(h, directory, xlim=7)
