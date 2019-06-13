4e7f8614cc0f36eae0f699394dc27db1a18f2e21
Sofia Sanz <sofiasw2@gmail.com>
2019-06-13 15:47:01 +0200

    maint: add general onsite energies of the TBHam

--- Hubbard/hamiltonian.py
+++ Hubbard/hamiltonian.py
@@ -81,18 +81,12 @@ class HubbardHamiltonian(object):
         for atom, ias in g.atoms.iter(True):
             # charge on neutral atom
             n0 = atom.Z - 5
-            if atom.Z == 5:
-                e0 = self.eB
-            elif atom.Z == 7:
-                e0 = self.eN
-            else:
-                e0 = 0.
 
             # Faster to do it for more than one element at a time
-            E[ias, :] += e0 - self.U * n0
+            E[ias, :] -= self.U * n0
 
             for ia in ias:
-                self.H[ia, ia, [0, 1]] = E[ia]
+                self.H[ia, ia, [0, 1]] = E[ia] + self.e0[ia]
 
     def random_density(self):
         """ Initialize spin polarization  with random density """
