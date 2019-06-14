d65fa781bd53f89c1d384cdf2280e5c0beaec8f3
Sofia Sanz <sofiasw2@gmail.com>
2019-06-14 14:27:01 +0200

    maint: reorganized test-hubbard-plots.py

--- tests/test-hubbard-plots.py
+++ tests/test-hubbard-plots.py
@@ -22,40 +22,43 @@ H.update_hamiltonian()
 p = plot.Charge(H, colorbar=True)
 p.savefig('chg.pdf')
 
-p = plot.Charge(H, realspace=True, colorbar=True)
-p.savefig('chg_rs.pdf')
-
 p = plot.ChargeDifference(H)
 p.savefig('chgdiff.pdf')
-
-p = plot.ChargeDifference(H, realspace=True)
-p.savefig('chgdiff_rs.pdf')
 
 p = plot.SpinPolarization(H)
 p.annotate()
 p.savefig('pol.pdf')
 
-p = plot.SpinPolarization(H, realspace=True)
-p.savefig('pol_rs.pdf')
-
 ev, evec = H.eigh(eigvals_only=False, spin=0)
-p = plot.Wavefunction(H, 500*evec[:, 10])
+p = plot.Wavefunction(H, 500*evec[:, 10], colorbar=True)
 p.savefig('wf.pdf')
-
-p = plot.Wavefunction(H, 10*evec[:, 10], realspace=True)
-p.savefig('wf_rs.pdf')
 
 p = plot.Spectrum(H)
 p.savefig('spectrum.pdf')
 
 p = plot.LDOSmap(H)
-p.savefig('ldos.pdf')
+p.savefig('ldos_map.pdf')
 
 p = plot.DOS_distribution(H, 0.15, f=300, sites=[60])
-p.savefig('dos.pdf')
+p.savefig('dos_dist.pdf')
 
 p = plot.DOS(H, np.linspace(-0.2,0.2,101))
-p.savefig('dos2.pdf')
+p.savefig('total_dos.pdf')
 
 p = plot.DOS(H, np.linspace(-0.2,0.2,101), sites=[60])
-p.savefig('ldos2.pdf')
+p.savefig('ldos.pdf')
+
+# Test real-space plots?
+if False:
+
+    p = plot.Charge(H, realspace=True, colorbar=True)
+    p.savefig('chg_rs.pdf')
+
+    p = plot.ChargeDifference(H, realspace=True)
+    p.savefig('chgdiff_rs.pdf')
+
+    p = plot.SpinPolarization(H, realspace=True)
+    p.savefig('pol_rs.pdf')
+
+    p = plot.Wavefunction(H, 10*evec[:, 10], realspace=True)
+    p.savefig('wf_rs.pdf')
