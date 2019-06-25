8152abea879c8c60a2a6737c38fd7560bfc1e9e8
Sofia Sanz <sofiasw2@gmail.com>
2019-06-25 19:38:32 +0200

    examples: new script to analyze all triangulenes

--- examples/molecules/triangulene/compute_FM-AFM.py
+++ examples/molecules/triangulene/compute_FM-AFM.py
@@ -5,44 +5,49 @@ import sys
 import numpy as np
 import sisl
 
+# Folder name (triangulene-1, -2 or -3)
+fn = sys.argv[1]
+
 # Build sisl Geometry object
-mol = sisl.get_sile('triangulene.xyz').read_geometry()
+mol = sisl.get_sile(fn+'/molecule.xyz').read_geometry()
 mol = mol.move(-mol.center(what='xyz'))
 mol.sc.set_nsc([1,1,1])
 
 # 3NN tight-binding model
 H = hh.HubbardHamiltonian(mol, t1=2.7, t2=.2, t3=.18)
 
-f = open('FM-AFM.dat', 'w')
+f = open(fn+'/FM-AFM.dat', 'w')
 
 for u in np.linspace(0.0, 3.5, 15):
     H.U = u
     # AFM case first
     try:
-        c = ncdf.read('triangulene.nc', ncgroup='AFM') # Try reading, if we already have density on file
+        c = ncdf.read(fn+'/triangulene.nc', ncgroup='AFM_U%i'%(u*100)) # Try reading, if we already have density on file
+        assert ncdf.gethash(H).hash == c.hash
         H.nup, H.ndn = c.nup, c.ndn
     except:
         H.random_density()
 
-    dn = H.converge(mix=.5, tol=1e-5)
+    dn = H.converge(tol=1e-10)
     eAFM = H.Etot
-    ncdf.write(H, 'triangulene.nc', ncgroup='AFM')
+    ncdf.write(H, fn+'/triangulene.nc', ncgroup='AFM_U%i'%(u*100))
 
     p = plot.SpinPolarization(H,  colorbar=True)
     p.annotate()
-    p.savefig('AFM-pol-%i.pdf'%(u*100))
+    p.savefig(fn+'/AFM-pol-%i.pdf'%(u*100))
 
     # Now FM case
     H.Nup += 1 # change to two more up-electrons than down
     H.Ndn -= 1
     try:
-        c = ncdf.read('triangulene.nc', ncgroup='FM') # Try reading, if we already have density on file
+        c = ncdf.read(fn+'/triangulene.nc', ncgroup='FM_U%i'%(u*100)) # Try reading, if we already have density on file
+        assert ncdf.gethash(H).hash == c.hash
         H.nup, H.ndn = c.nup, c.ndn
     except:
         H.random_density()
-    dn = H.converge(mix=.5, tol=1e-5)
+    dn = H.converge(tol=1e-10)
     eFM = H.Etot
-    ncdf.write(H, 'triangulene.nc', ncgroup='FM')
+    ncdf.write(H, fn+'/triangulene.nc', ncgroup='FM_U%i'%(u*100))
 
     # Revert the imbalance for next loop
     H.Nup -= 1
