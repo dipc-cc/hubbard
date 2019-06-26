df8e041924ecd511fdf9f43e9f7261a299358ef7
Sofia <sofiasw2@gmail.com>
2019-06-26 19:48:30 +0200

    examples: updated and set correct number of neutral atoms by hand

--- examples/molecules/2B-7AGNR/compute_5x5.py
+++ examples/molecules/2B-7AGNR/compute_5x5.py
@@ -1,5 +1,6 @@
 import Hubbard.hamiltonian as hh
 import Hubbard.plot as plot
+import Hubbard.sp2 as sp2
 import sys
 import numpy as np
 import Hubbard.ncdf as ncdf
@@ -11,11 +12,14 @@ pol = True
 mol = sisl.get_sile('7AGNR2B_5x5.XV').read_geometry()
 mol.sc.set_nsc([1,1,1])
 mol = mol.move(-mol.center(what='xyz'))
+H_mol = sp2(mol, t1=2.7, t2=0.2, t3=0.18, dim=2)
 
 pol_up = [[3, 99], [3, 178], [3, 82]]
 pol_dn = [[82, 178], [82, 99], [99, 178]]
 for ig, grp in enumerate(['AFM-AFM-AFM', 'AFM-FM-AFM', 'FM-AFM-FM']):
-    H = hh.HubbardHamiltonian(mol, t1=2.7, t2=0.2, t3=.18, U=5.0)
+    H = hh.HubbardHamiltonian(H_mol, U=5.0)
+    H.Nup -= 1
+    H.Ndn -= 1
     try:
         # Try reading from file
         calc = ncdf.read('7AGNR2B_5x5.nc', ncgroup=grp)
