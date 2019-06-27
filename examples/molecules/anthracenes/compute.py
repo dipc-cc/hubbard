3535bf22e23e7f203a532db4d0b70b12651a82be
Sofia <sofiasw2@gmail.com>
2019-06-27 16:14:51 +0200

    examples: updated anthracenes/compute.py

--- examples/molecules/anthracenes/compute.py
+++ examples/molecules/anthracenes/compute.py
@@ -1,17 +1,19 @@
 import Hubbard.hamiltonian as hh
+import Hubbard.sp2 as sp2
 import sys
 import numpy as np
 import Hubbard.ncdf as ncdf
 import sisl
 
 # Build sisl Geometry object
-mol_file = '2-anthracene.XV'
-mol = sisl.get_sile(mol_file).read_geometry()
+mol_file = '2-anthracene'
+mol = sisl.get_sile(mol_file+'.XV').read_geometry()
 mol.sc.set_nsc([1,1,1])
 mol = mol.move(-mol.center(what='xyz'))
 
 # 3NN tight-binding model
-H = hh.HubbardHamiltonian(mol, t1=2.7, t2=0.2, t3=.18)
+Hsp2 = sp2(mol, t1=2.7, t2=0.2, t3=.18, dim=2)
+H = hh.HubbardHamiltonian(Hsp2)
 
 # Output file to collect the energy difference between
 # FM and AFM solutions
@@ -23,25 +25,25 @@ for u in np.linspace(0.0, 4.0, 5):
 
     # AFM case first
     try:
-        c = ncdf.read(mol_file[:-3]+'.nc', ncgroup='AFM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
+        c = ncdf.read(mol_file+'.nc', ncgroup='AFM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
         H.nup, H.ndn = c.nup, c.ndn
     except:
         H.random_density()
     dn = H.converge()
     eAFM = H.Etot
-    ncdf.write(H, mol_file[:-3]+'.nc', ncgroup='AFM_U%i'%(H.U*1000))
+    ncdf.write(H, mol_file+'.nc', ncgroup='AFM_U%i'%(H.U*1000))
     
     # Now FM case
     H.Nup += 1 # change to two more up-electrons than down
     H.Ndn -= 1
     try:
-        c = ncdf.read(mol_file[:-3]+'.nc', ncgroup='FM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
+        c = ncdf.read(mol_file+'.nc', ncgroup='FM_U%i'%(H.U*1000)) # Try reading, if we already have density on file
         H.nup, H.ndn = c.nup, c.ndn
     except:
         H.random_density()
     dn = H.converge()
     eFM = H.Etot
-    ncdf.write(H, mol_file[:-3]+'.nc', ncgroup='FM_U%i'%(H.U*1000))
+    ncdf.write(H, mol_file+'.nc', ncgroup='FM_U%i'%(H.U*1000))
     
     # Revert the imbalance for next loop
     H.Nup -= 1
