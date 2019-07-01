999257a2595100d267cc9a8ed9bd432dde90af8c
Sofia Sanz <sofiasw2@gmail.com>
2019-07-01 17:38:31 +0200

    enh: use previous density if not found in density file

--- examples/molecules/triangulene/compute_FM-AFM.py
+++ examples/molecules/triangulene/compute_FM-AFM.py
@@ -16,17 +16,21 @@ mol.sc.set_nsc([1,1,1])
 
 # 3NN tight-binding model
 Hsp2 = sp2(mol, t1=2.7, t2=0.2, t3=.18, dim=2)
+H = hh.HubbardHamiltonian(Hsp2)
 f = open(fn+'/FM-AFM.dat', 'w')
 
-for u in np.linspace(0.0, 5.0, 21):
-    H = hh.HubbardHamiltonian(Hsp2)
+nup_AFM, ndn_AFM = H.nup*1, H.ndn*1
+nup_FM, ndn_FM = H.nup*1, H.ndn*1
+for u in np.linspace(5.0, 0.0, 21):
 
     H.U = u
     # AFM case first
+    H.nup, H.ndn = nup_AFM, ndn_AFM
     H.read_density(fn+'/triangulene-AFM.nc')
     dn = H.converge(tol=1e-10)
     eAFM = H.Etot
     H.write_density(fn+'/triangulene-AFM.nc')
+    nup_AFM, ndn_AFM = H.nup*1, H.ndn*1
 
     p = plot.SpinPolarization(H,  colorbar=True, vmax=0.4)
     p.annotate()
@@ -36,10 +40,12 @@ for u in np.linspace(0.0, 5.0, 21):
     H.Nup += 1 # change to two more up-electrons than down
     H.Ndn -= 1
     
+    H.nup, H.ndn = nup_FM, ndn_FM
     H.read_density(fn+'/triangulene-FM.nc')
     dn = H.converge(tol=1e-10)
     eFM = H.Etot
     H.write_density(fn+'/triangulene-FM.nc')
+    nup_FM, ndn_FM = H.nup*1, H.ndn*1
 
     p = plot.SpinPolarization(H,  colorbar=True, vmax=0.4)
     p.annotate()
