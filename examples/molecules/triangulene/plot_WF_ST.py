324eda71b22e479a66afb9cf1cf2f7729633d10d
Sofia Sanz <sofiasw2@gmail.com>
2019-07-03 15:06:30 +0200

    enh: plot wavefunctions of correct groundstate (FM for triangulene-1 and -2)

--- examples/molecules/triangulene/plot_WF_ST.py
+++ examples/molecules/triangulene/plot_WF_ST.py
@@ -16,23 +16,32 @@ mol.sc.set_nsc([1,1,1])
 
 # 3NN tight-binding model
 Hsp2 = sp2(mol, t1=2.7, t2=0.2, t3=.18, dim=2)
+H = hh.HubbardHamiltonian(Hsp2)
+
+if 'triangulene-1' in fn or 'triangulene-2' in fn:
+    H.Nup += 1
+    H.Ndn -= 1
+    fn_nc = fn+'triangulene-FM.nc'
+else:
+    fn_nc = fn+'trianglene-AFM.nc'
+H.polarize_sublattices()
 
 for u in [0., 3.5]:
 
-    H = hh.HubbardHamiltonian(Hsp2, U=u)
-
-    # Plot wavefunctions
-    try:
-        c = ncdf.read(molecule+'/triangulene.nc', ncgroup='AFM_U%i'%(int(H.U*100))) # Try reading, if we already have density on file
-        H.nup, H.ndn = c.nup, c.ndn
-    except:
-        H.random_density()
+    H.U = u
+    H.read_density(fn_nc)
     
+    # Plot wavefunctions
     dn = H.converge()
+    H.find_midgap()
     ev_up, evec_up = H.eigh(eigvals_only=False, spin=0)
     ev_up -= H.midgap
     ev_dn, evec_dn = H.eigh(eigvals_only=False, spin=1)
     ev_dn -= H.midgap
+
+    p = plot.Wavefunction(H, 3000*evec_up[:, H.Nup-2])
+    p.set_title('$E_{\uparrow}=%.2f$ meV, $U=%.1f$ eV'%(ev_up[H.Nup-2]*1000, H.U))
+    p.savefig(fn+'/U%i_state%i_up.pdf'%(H.U*100, H.Nup-2))
 
     p = plot.Wavefunction(H, 3000*evec_up[:, H.Nup-1])
     p.set_title('$E_{\uparrow}=%.2f$ meV, $U=%.1f$ eV'%(ev_up[H.Nup-1]*1000, H.U))
