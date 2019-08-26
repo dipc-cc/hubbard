98daeb22857b7249725167080204b8ab74f8c6c4
Sofia Sanz <sofiasw2@gmail.com>
2019-08-26 17:46:44 +0200

    tests: add test of add_H function

--- tests/test-hubbard-geom.py
+++ tests/test-hubbard-geom.py
@@ -13,5 +13,8 @@ for w in [8, 9]:
             geometry = geom.cgnr(n, m, w)
             geometry.tile(2, axis=0).write('cgnr-%i-%i-%i.xyz' % (n, m, w))
 
+geometry = geom.add_Hatoms(geometry, sp3=[15])
+geometry.tile(2,axis=0).write('cgnr-H.xyz')
+
 geometry = geom.ssh()
 geometry.tile(2, axis=0).write('ssh.xyz')
