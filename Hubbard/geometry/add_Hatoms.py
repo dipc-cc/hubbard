353f2817ff294b1a2655dd3bde10fc9689ef4fe0
Sofia Sanz <sofiasw2@gmail.com>
2019-08-26 17:37:55 +0200

    enh: added possibility to include sp3 configuration

--- Hubbard/geometry/add_Hatoms.py
+++ Hubbard/geometry/add_Hatoms.py
@@ -13,7 +13,7 @@ from __future__ import print_function
 import numpy as np
 import sisl
 
-def add_Hatoms(geom, d=1.1):
+def add_Hatoms(geom, d=1.1, sp3=[]):
     ''' Function to saturate edge C atoms with Hydrogen'''
     Hxyz = []
     for ia in geom:
@@ -25,5 +25,12 @@ def add_Hatoms(geom, d=1.1):
             p = -1*(v1 + v2)
             p = p * d / ( (p**2).sum() )**0.5
             Hxyz.append(p+geom.xyz[ia, :])
+            if ia in sp3:
+                Hxyz[-1][2] -= 0.9
+                p = Hxyz[-1]-geom.xyz[ia, :]
+                p = p * d / ( (p**2).sum() )**0.5
+                Hxyz[-1] = p + geom.xyz[ia, :]
+                Hxyz.append(Hxyz[-1]*1.0)
+                Hxyz[-1][2] -= 2*Hxyz[-1][2]
     Hgeom = sisl.Geometry(Hxyz, atom=sisl.Atom(Z=1, R=d), sc=geom.sc)
     return geom.add(Hgeom)
