605fb91cf8a97d3c3e8372382d4ac21f4d1aa6f8
Sofia Sanz <sofiasw2@gmail.com>
2019-08-29 15:22:17 +0200

    systems: added H atoms to pol-plots in triangulene example

--- examples/molecules/triangulene/compute_FM-AFM.py
+++ examples/molecules/triangulene/compute_FM-AFM.py
@@ -1,6 +1,7 @@
 import Hubbard.hamiltonian as hh
 import Hubbard.plot as plot
 import Hubbard.sp2 as sp2
+import Hubbard.geometry as geom
 import sys
 import numpy as np
 import sisl
@@ -12,6 +13,7 @@ fn = sys.argv[1]
 mol = sisl.get_sile(fn+'/molecule.xyz').read_geometry()
 mol = mol.move(-mol.center(what='xyz'))
 mol.sc.set_nsc([1,1,1])
+mol = geom.add_Hatoms(mol)
 
 # 3NN tight-binding model
 Hsp2 = sp2(mol, t1=2.7, t2=0.2, t3=.18, dim=2)
@@ -35,7 +37,7 @@ for u in np.linspace(5.0, 0.0, 21):
     H.write_density(ncf)
     nup_AFM, ndn_AFM = H.nup*1, H.ndn*1
 
-    p = plot.SpinPolarization(H,  colorbar=True, vmax=0.4)
+    p = plot.SpinPolarization(H, ext_geom=mol, colorbar=True, vmax=0.4)
     p.annotate()
     p.savefig(fn+'/AFM-pol-%i.pdf'%(u*100))
     p.close()
@@ -52,7 +54,7 @@ for u in np.linspace(5.0, 0.0, 21):
     H.write_density(ncf)
     nup_FM, ndn_FM = H.nup*1, H.ndn*1
 
-    p = plot.SpinPolarization(H,  colorbar=True, vmax=0.4)
+    p = plot.SpinPolarization(H, ext_geom=mol, colorbar=True, vmax=0.4)
     p.annotate()
     p.savefig(fn+'/FM-pol-%i.pdf'%(u*100))
     p.close()
