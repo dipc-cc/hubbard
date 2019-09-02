2dd70d5a2e37fa6232faa1ee96b1c2a26981dc9b
Sofia Sanz <sofiasw2@gmail.com>
2019-09-02 17:02:36 +0200

    triangulene: do also H-passivated/2H trianguelne molecule in figS5.py

--- examples/molecules/triangulene/triangulene-2/figS5.py
+++ examples/molecules/triangulene/triangulene-2/figS5.py
@@ -5,8 +5,43 @@ import sys
 import numpy as np
 import sisl
 
+# First do H-passivation/2H-pos-1-2 molecule
+
+# Build sisl Geometry object
+fn = 'H-passivation/2H-pos-1-2/'
+mol = sisl.get_sile(fn+'/molecule.xyz').read_geometry()
+mol = mol.move(-mol.center(what='xyz'))
+mol.sc.set_nsc([1,1,1])
+
+# 3NN tight-binding model
+Hsp2 = sp2(mol, t1=2.7, t2=0.2, t3=.18, dim=2)
+H = hh.HubbardHamiltonian(Hsp2)
+H.polarize_sublattices()
+H.U = 3.0
+ncf = fn+'/triangulene.nc'
+H.read_density(ncf)
+dn = H.converge(tol=1e-10, fn=ncf)
+H.write_density(ncf)
+
+# Plot spin polarization
+p = plot.SpinPolarization(H, ext_geom=mol, colorbar=True, vmax=0.4)
+p.axes.axis('off')
+p.savefig(fn+'/pol-%i.pdf'%(H.U*100))
+
+# Plot DOS at HOMO 
+ev_up, evec_up = H.eigh(spin=0, eigvals_only=False)
+ev_dn, evec_dn = H.eigh(spin=1, eigvals_only=False)
+H.find_midgap()
+ev_up -= H.midgap
+ev_dn -= H.midgap
+HOMO = max(ev_up[H.Nup-1], ev_dn[H.Ndn-1])
+p = plot.DOS_distribution(H, ext_geom=mol, E=HOMO, realspace=True)
+p.set_title('E $= %.2f$ eV'%(HOMO), fontsize=30)
+p.axes.axis('off')
+p.savefig(fn+'/U%i_DOS.pdf'%(H.U*100))
+
+# Then do the rest of H-passivated molecules
 p = plot.Plot()
-pos = [21,18,28,32,34]
 for i, fn in enumerate(['pos-1/', 'pos-2/', 'pos-3/', 'pos-4/', 'pos-5/']):
     # Build sisl Geometry object
     fn = 'H-passivation/'+fn
@@ -28,7 +63,6 @@ for i, fn in enumerate(['pos-1/', 'pos-2/', 'pos-3/', 'pos-4/', 'pos-5/']):
         H.U = u
         U.append(u)
 
-        # AFM case first
         ncf = fn+'/triangulene.nc'
         H.nup, H.ndn = nup, ndn
         H.read_density(ncf)
@@ -40,11 +74,25 @@ for i, fn in enumerate(['pos-1/', 'pos-2/', 'pos-3/', 'pos-4/', 'pos-5/']):
         
         if u == 3.0:
             p_pol = plot.SpinPolarization(H, ext_geom=mol, colorbar=True, vmax=0.4)
-            #p.annotate()
-            p_pol.axes.set_title('pos-%i'%(i+1), fontsize=50, y=-0.2)
             p_pol.axes.axis('off')
             p_pol.savefig(fn+'/pol-%i.pdf'%(u*100))
             p_pol.close()
+
+            if i==2:
+                # Plot DOS at HOMO for pos-3 H passivated molecule
+                ev_up, evec_up = H.eigh(spin=0, eigvals_only=False)
+                ev_dn, evec_dn = H.eigh(spin=1, eigvals_only=False)
+                H.find_midgap()
+                if (H.Nup+H.Ndn) %2 != 0:
+                    H.midgap = max(ev_up[H.Nup-1], ev_dn[H.Ndn-1])
+                ev_up -= H.midgap
+                ev_dn -= H.midgap
+                HOMO = max(ev_up[H.Nup-1], ev_dn[H.Ndn-1])
+                p_dos = plot.DOS_distribution(H, ext_geom=mol, E=HOMO, realspace=True)
+                p_dos.set_title('E $= %.2f$ eV'%(HOMO), fontsize=30)
+                p_dos.axes.axis('off')
+                p_dos.savefig(fn+'/U%i_DOS.pdf'%(H.U*100))
+                p_dos.close()
 
         f.write('%.4f %.8f\n'%(H.U, e))
 
@@ -54,6 +102,7 @@ for i, fn in enumerate(['pos-1/', 'pos-2/', 'pos-3/', 'pos-4/', 'pos-5/']):
     else:
         p.axes.plot(U, E-E0, 'o', label='pos$_{%i}$-pos$_{1}$'%(i+1))
         p.set_xlabel(r'U [eV]', fontsize=25)
+        p.axes.set_xticks([0,1,2,3,4])
         p.axes.set_xticklabels(p.axes.get_xticks(), fontsize=20)
         p.axes.set_yticklabels(['%.1f'%i for i in p.axes.get_yticks()], fontsize=20)
         p.set_ylabel(r'E$_{i}$-E$_{1}$ [eV]', fontsize=25)
