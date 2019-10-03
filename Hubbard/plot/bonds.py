166b02830fde17e8cb2985f6d1d719683ef737c3
Sofia <sofiasw2@gmail.com>
2019-10-03 11:01:53 +0200

    enh: plot the hopping integrals between atoms

--- Hubbard/plot/bonds.py
+++ Hubbard/plot/bonds.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import matplotlib.pyplot as plt
 from Hubbard.plot import GeometryPlot
+from Hubbard.plot import Plot
 import numpy as np
 
 
@@ -47,3 +48,23 @@ class BondOrder(GeometryPlot):
                 y = [xr[1], xr[1]-R[1]/2]
                 plt.plot(x, y, c='k', ls='-', lw=4, solid_capstyle='round')
             self.axes.text(sum(x)/2, sum(y)/2, r'%.3f'%BO[r, c], ha="center", va="center", rotation=15, size=6, bbox=bbox_props)
+
+class bonds_hoppings(Plot):
+
+    def __init__(self, H, **keywords):
+        
+        Plot.__init__(self, **keywords)
+
+        for ia in H.geom:
+            x0, y0 = H.geom.xyz[ia, 0], H.geom.xyz[ia, 1] 
+            for ib in H.geom:
+                x1, y1 = H.geom.xyz[ib, 0], H.geom.xyz[ib, 1] 
+                t = H[ia, ib][0,0]
+                if abs(t) == 2.7:
+                    plt.plot([x0,x1], [y0,y1], '-', markersize=2, color='blue', linewidth=1.2)
+                elif abs(t) == 0.2:
+                    plt.plot([x0,x1], [y0,y1], '--', markersize=2, color='red', linewidth=1.2)
+                elif abs(t) == 0.18:
+                    plt.plot([x0,x1], [y0,y1], '--', markersize=2, color='green', linewidth=1.2)
+        
+        plt.axis('off')
