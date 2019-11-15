c9471873cf140e7d68500ddbff0486140cdf3b32
sofiasanz <sofiasw2@gmail.com>
2019-11-15 13:48:36 +0100

    benchmark: added AGNR constriction to test iterate3 method

---
+++ examples/open/benchmarks/AGNR-constriction/setup.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+import sisl
+import numpy as np
+import Hubbard.geometry as geometry
+import Hubbard.hamiltonian as hh
+import Hubbard.sp2 as sp2
+import Hubbard.plot as plot
+import os
+
+'''
+This script benchmarks the 9-AGNR constriction of ref. [Y. Hancock, et al., PRB 81, 245402 (2010)]
+i.e. the system of fig. 3(b) and the transmissions shown in fig. 4(a)
+To describe the system model D of this paper is used (t1=2.7, t2=0.2, t3=0.18, U=2.0 eV)
+'''
+
+# Set U for the whole calculation
+U = 2.0
+
+# Build zigzag GNR
+AGNR = geometry.agnr(9)
+
+# and 3NN TB Hamiltonian
+H_elec = sp2(AGNR, t1=2.7, t2=0.2, t3=0.18)
+
+# Hubbard Hamiltonian of elecs
+MFH_elec = hh.HubbardHamiltonian(H_elec, U=U, nkpt=[102, 1, 1])
+
+# Hubbard Hamiltonian of elecs
+MFH_elec = hh.HubbardHamiltonian(H_elec, U=U, nkpt=[102, 1, 1])
+# Initial densities
+MFH_elec.read_density('elec_density.nc')
+
+# Converge Electrode Hamiltonians and write to netcdf file
+dn = MFH_elec.converge(method=2)
+MFH_elec.H.write('MFH_elec.nc')
+# Write also densities for future calculations
+MFH_elec.write_density('elec_density.nc')
+# Plot spin polarization of electrodes
+p = plot.SpinPolarization(MFH_elec, colorbar=True)
+p.savefig('spin_elecs.pdf')
+
+# Find Fermi level of reservoirs
+dist = sisl.get_distribution('fermi_dirac', smearing=0.025)
+Ef_elecs = MFH_elec.H.fermi_level(MFH_elec.mp, q=[MFH_elec.Nup, MFH_elec.Ndn], distribution=dist)
+
+# Central region is a repetition of the electrodes without PBC
+HC = H_elec.tile(11,axis=0)
+HC = HC.remove([89,106,107,86,85,102,105,104,103,124,120,101,100,123])
+HC.set_nsc([1,1,1])
+HC.geom.write('device.xyz')
+
+# Map electrodes in the device region
+elec_indx = [range(len(H_elec)), range(len(HC.H)-len(H_elec), len(HC.H))]
+
+# MFH object
+MFH_HC = hh.HubbardHamiltonian(HC.H, U=U, elecs=MFH_elec, elec_indx=elec_indx)
+# Initial densities
+MFH_HC.read_density('HC_density.nc')
+
+# Converge using iterative method 3 and write Hamiltonian into netcdf file
+dn = MFH_HC.converge(method=3, steps=1, tol=1e-5)
+MFH_HC.H.write('MFH_HC.nc')
+print('Nup, Ndn: ', MFH_HC.nup.sum(), MFH_HC.ndn.sum())
+# Write also densities for future calculations
+MFH_HC.write_density('HC_density.nc')
+# Plot spin polarization of electrodes
+p = plot.SpinPolarization(MFH_HC, colorbar=True)
+p.savefig('spin_HC.pdf')
+
+# RUN TBtrans and plot transmissions
+print('Clean TBtrans output from previous run')
+os.system('rm device.TBT*')
+os.system('rm fdf*')
+print('Running TBtrans')
+os.system('tbtrans RUN.fdf > RUN.out')
+
+tbt_up = sisl.get_sile('device.TBT_UP.nc')
+tbt_dn = sisl.get_sile('device.TBT_DN.nc')
+
+p = plot.Plot()
+p.axes.plot(tbt_up.E-Ef_elecs[0], tbt_up.transmission(0,1), color='k', label=r'$\sigma=\uparrow$')
+p.axes.plot(tbt_dn.E-Ef_elecs[1], tbt_dn.transmission(0,1), '--', color='r', label=r'$\sigma=\downarrow$')
+p.axes.legend()
+p.set_xlim(-2,2)
+p.set_xlabel('Energy [eV]')
+p.set_ylabel('Transmission [a.u.]')
+p.savefig('transmission.pdf')
