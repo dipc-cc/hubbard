e011aae9f2b9d794eff298ba87ee5e436be19e6b
sofiasanz <sofiasw2@gmail.com>
2019-11-28 14:51:12 +0100

    test: shift electrodes and device hamiltonians with their respective Fermi levels

--- tests/test-transmission-open.py
+++ tests/test-transmission-open.py
@@ -15,18 +15,19 @@ U = 3.0
 ZGNR = geometry.zgnr(2)
 
 # and 3NN TB Hamiltonian
-H_elec = sp2(ZGNR, t1=2.7, t2=0.2, t3=0.18).tile(2,axis=0)
+H_elec = sp2(ZGNR, t1=2.7, t2=0.2, t3=0.18)
 
 # Hubbard Hamiltonian of elecs
 MFH_elec = hh.HubbardHamiltonian(H_elec, U=U, nkpt=[102, 1, 1])
 
-# Converge Electrode Hamiltonians and write it to netcdf file
+# Converge Electrode Hamiltonians
 dn = MFH_elec.converge(method=2)
-MFH_elec.H.write('MFH_elec.nc')
 
-# Obtain Fermi level of reservoirs
 dist = sisl.get_distribution('fermi_dirac', smearing=0.025)
-Ef_elecs = MFH_elec.H.fermi_level(MFH_elec.mp, q=[MFH_elec.Nup, MFH_elec.Ndn], distribution=dist)
+Ef_elec = MFH_elec.H.fermi_level(MFH_elec.mp, q=[MFH_elec.Nup, MFH_elec.Ndn], distribution=dist)
+# Shift each electrode with its Fermi-level and write it to netcdf file
+MFH_elec.H.shift(-Ef_elec)
+MFH_elec.H.write('MFH_elec.nc')
 
 # Central region is a repetition of the electrodes without PBC
 HC = H_elec.tile(3,axis=0)
@@ -42,8 +43,12 @@ MFH_HC = hh.HubbardHamiltonian(HC.H, DM=MFH_elec.DM.tile(3,axis=0), U=U, elecs=[
 dn = MFH_HC.converge(method=3, steps=1, tol=1e-5)
 print('Nup, Ndn: ', MFH_HC.nup.sum(), MFH_HC.ndn.sum())
 
-# Shift device with Fermi level of reservoirs
+# Shift device with its Fermi level and write nc file
+MFH_HC.H.shift(-MFH_HC.Ef)
 MFH_HC.H.write('MFH_HC.nc')
+
+print(abs(MFH_elec.tile(3,axis=0).nup - MFH_HC.nup).sum())
+print(abs(MFH_elec.tile(3,axis=0).ndn - MFH_HC.ndn).sum())
 
 # TBtrans RUN and plot transmission
 import os
@@ -57,8 +62,8 @@ tbt_up = sisl.get_sile('device.TBT_UP.nc')
 tbt_dn = sisl.get_sile('device.TBT_DN.nc')
 
 p = plot.Plot()
-p.axes.plot(tbt_up.E-Ef_elecs[0], tbt_up.transmission(0,1), label=r'$\sigma=\uparrow$')
-p.axes.plot(tbt_up.E-Ef_elecs[1], tbt_dn.transmission(0,1), label=r'$\sigma=\downarrow$')
+p.axes.plot(tbt_up.E, tbt_up.transmission(0,1), label=r'$\sigma=\uparrow$')
+p.axes.plot(tbt_dn.E, tbt_dn.transmission(0,1), label=r'$\sigma=\downarrow$')
 p.axes.legend()
 p.set_xlim(-10,10)
 p.set_xlabel('Energy [eV]')
