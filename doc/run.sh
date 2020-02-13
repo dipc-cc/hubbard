7877a9a3ccf00426cea2ad18173fc7ae20fb6855
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2020-02-13 15:20:59 +0100

    doc: include comment on required py3 packages

--- doc/run.sh
+++ doc/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#sudo apt install python3-sphinx python3-numpydoc python3-sphinx-rtd-theme
 rm -rf api-gen latest
 make clean
 sphinx-apidoc -fMeET -o api-gen ../Hubbard ../Hubbard/**/setup.py
