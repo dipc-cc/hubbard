05ec5295829bf32856dbd18d9b735367725f195f
Sofia Sanz <sofiasw2@gmail.com>
2020-02-17 09:38:29 +0100

    tests: updated test-hubbard-quick

--- tests/test-hubbard-quick.py
+++ tests/test-hubbard-quick.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import Hubbard.hamiltonian as hh
 import Hubbard.sp2 as sp2
+import Hubbard.density as dm
 import sisl
 
 # Build sisl Geometry object
@@ -12,17 +13,17 @@ molecule.sc.set_nsc([1,1,1])
 Hsp2 = sp2(molecule)
 H = hh.HubbardHamiltonian(Hsp2, U=3.5)
 H.random_density()
-dn = H.iterate(mix=.1)
+dn = H.iterate(dm.dm_insulator,mix=.1)
 print(dn, H.Etot)
 
 # Run also one iteration with data from ncfile
 H.read_density('mol-ref/density.nc')
-dn = H.iterate(mix=1)
+dn = H.iterate(dm.dm_insulator, mix=1)
 etot = 1*H.Etot
 print(dn, etot)
 
 # Test iterate2 method
-d = H.iterate2(mix=1)
+d = H.iterate(dm.dm, mix=1)
 e = H.Etot
 print(d-dn, e-etot)
 
