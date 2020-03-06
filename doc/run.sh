41c64613e6fee7b522d297b76e6f7dbc7bf41fae
Sofia Sanz <sofiasw2@gmail.com>
2020-03-06 19:31:00 +0100

    doc: modified sphinx-apidoc options

--- doc/run.sh
+++ doc/run.sh
@@ -3,8 +3,8 @@
 #sudo apt install python3-sphinx python3-numpydoc python3-sphinx-rtd-theme
 rm -rf api-gen latest
 make clean
-sphinx-apidoc -fMeET -o api-gen ../Hubbard ../Hubbard/**/setup.py
+sphinx-apidoc -fFMeT -o api-gen ../Hubbard ../Hubbard/**/setup.py
 make html
 rm -r _build/html/_sources
 mv _build/html latest
-rm -rf api-gen _build _static _templates
+rm -rf api-gen _build _static
