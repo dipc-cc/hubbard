bfc2fadd945ce07a12437c1272143ed9eed0208d
Sofia Sanz <sofiasw2@gmail.com>
2020-03-11 18:09:42 +0100

    enh: generalized bandstructure plotting class to (1D) systems periodic along all posible directions (x,y,z)

--- Hubbard/plot/bandstructure.py
+++ Hubbard/plot/bandstructure.py
@@ -10,7 +10,7 @@ __all__ = ['Bandstructure']
 class Bandstructure(Plot):
     """ Plot the bandstructure for the `HubbardHamiltonian` object of a 1D system """
 
-    def __init__(self, HubbardHamiltonian, nk=51, ymax=4., projection=None, spin=0, scale=1, c='r', **keywords):
+    def __init__(self, HubbardHamiltonian, ymax=4., projection=None, spin=0, scale=1, c='r', **keywords):
 
         # Set default keywords
         if 'figsize' not in keywords:
@@ -22,17 +22,17 @@ class Bandstructure(Plot):
         self.set_ylabel(r'$E_{nk}-E_\mathrm{mid}$ (eV)')
         self.set_ylim(-ymax, ymax)
 
-        self.add_bands(HubbardHamiltonian, nk=nk, projection=projection, spin=spin, scale=scale, c=c)
+        self.add_bands(HubbardHamiltonian, projection=projection, spin=spin, scale=scale, c=c)
 
-    def add_bands(self, HubbardHamiltonian, nk=51, projection=None, spin=0, scale=1, c='r'):
+    def add_bands(self, HubbardHamiltonian, projection=None, spin=0, scale=1, c='r'):
         # Get TB bands
-        ka = np.linspace(0, 0.5, nk)
+        ka = HubbardHamiltonian.mp.k
         ev = np.empty([len(ka), 2, HubbardHamiltonian.sites])
         pdos = np.empty([len(ka), 2, HubbardHamiltonian.sites])
         # Loop over k
         for ik, k in enumerate(ka):
             for ispin in range(2):
-                ev[ik, ispin], evec = HubbardHamiltonian.eigh([k, 0, 0], eigvals_only=False, spin=ispin)
+                ev[ik, ispin], evec = HubbardHamiltonian.eigh(k, eigvals_only=False, spin=ispin)
                 if projection != None:
                     v = evec[projection]
                     pdos[ik, ispin] = np.diagonal(np.dot(np.conjugate(v).T, v).real)
@@ -49,6 +49,7 @@ class Bandstructure(Plot):
             for i, evi in enumerate(ev[0, 0]):
                 plt.errorbar(x, ev[:, 0, i], yerr=scale*pdos[:, 0, i], alpha=.4, color='Grey')
         # Add spin-up component to plot (top layer)
-        plt.plot(x, ev[:, 0], c)
+        x = np.linspace(0, 0.5, len(ka))
+        plt.plot(x, ev[:, 0], color=c)
         # Adjust borders
         plt.subplots_adjust(left=0.2, top=.95, bottom=0.1, right=0.95)
