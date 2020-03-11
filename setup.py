52940e3afeab417df55c5a8068d2388096178eb7
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2020-03-11 12:11:53 +0100

    Release v0.0.2

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ def git_version():
     # Default release info
     MAJOR = 0
     MINOR = 0
-    MICRO = 1
+    MICRO = 2
     VERSION = [MAJOR, MINOR, MICRO]
     # Git revision prior to release:
-    GIT_REVISION = "unknown"
+    GIT_REVISION = "e97014e7f402ade5c6b18c03454b78862d13ea6e"
     GIT_LABEL = '.'.join(map(str, [MAJOR, MINOR, MICRO]))
 
     def _minimal_ext_cmd(cmd):
