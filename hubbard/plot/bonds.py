18ff67b1280d22afd8aa0821cd1fdbcfa29b5370
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2021-05-06 17:46:41 +0200

    maint: use axes.set_aspect(equal) + other polishing

--- hubbard/plot/bonds.py
+++ hubbard/plot/bonds.py
@@ -22,7 +22,8 @@ class BondOrder(GeometryPlot):
         if 'cmap' not in kwargs:
             kwargs['cmap'] = plt.cm.bwr
 
-        super().__init__(HH, **kwargs)
+        super().__init__(HH.geometry, **kwargs)
+
         bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.9)
 
         # Compute Huckel bond orders
@@ -35,8 +36,8 @@ class BondOrder(GeometryPlot):
             xr = HH.geometry.xyz[r]
             c = col[i]
             xc = HH.geometry.xyz[c]
-            R = xr-xc
-            if np.dot(R, R)**.5 <= d:
+            R = xr - xc
+            if np.dot(R, R) ** 0.5 <= d:
                 # intracell bond
                 x = [xr[0], xc[0]]
                 y = [xr[1], xc[1]]
@@ -48,14 +49,14 @@ class BondOrder(GeometryPlot):
                 P = np.dot(cell, R)
                 # Normalize
                 for i in range(3):
-                    P[i] /= np.dot(cell[i], cell[i])**.5
+                    P[i] /= np.dot(cell[i], cell[i]) ** 0.5
                 # Look up largest projection
                 j = np.argmax(np.abs(P))
-                R -= np.sign(P[j])*cell[j] # Subtract relevant lattice vector
-                x = [xr[0], xr[0]-R[0]/2]
-                y = [xr[1], xr[1]-R[1]/2]
+                R -= np.sign(P[j]) * cell[j] # Subtract relevant lattice vector
+                x = [xr[0], xr[0] - R[0] / 2]
+                y = [xr[1], xr[1] - R[1] / 2]
                 self.axes.plot(x, y, c='k', ls='-', lw=4, solid_capstyle='round')
-            self.axes.text(sum(x)/2, sum(y)/2, r'%.3f'%BO[r, c], ha="center", va="center", rotation=15, size=6, bbox=bbox_props)
+            self.axes.text(sum(x)/2, sum(y)/2, r'%.3f' % BO[r, c], ha="center", va="center", rotation=15, size=6, bbox=bbox_props)
 
 
 class BondHoppings(Plot):
@@ -79,17 +80,20 @@ class BondHoppings(Plot):
         cmap_e = kwargs.get("cmap_e", cmap)
 
         super().__init__(**kwargs)
+
         H = H.H.copy()
         H.set_nsc([1, 1, 1])
 
         # Create colormaps
-        tmax = kwargs.get("tmax", np.amax(abs(H.Hk())))
-        tmin = kwargs.get("tmin", np.amin(abs(H.Hk())))
+        tmp = abs(H.Hk())
+        tmax = kwargs.get("tmax", np.amax(tmp))
+        tmin = kwargs.get("tmin", np.amin(tmp))
         norm = mp.colors.Normalize(vmin=tmin, vmax=tmax)
         cmap_t = mp.cm.ScalarMappable(norm=norm, cmap=cmap_t)
 
-        emax = kwargs.get("emax", np.amax(abs(H.H.tocsr(0).diagonal())))
-        emin = kwargs.get("emin", np.amin(abs(H.H.tocsr(0).diagonal())))
+        tmp = abs(H.H.tocsr(0).diagonal())
+        emax = kwargs.get("emax", np.amax(tmp))
+        emin = kwargs.get("emin", np.amin(tmp))
         norm = mp.colors.Normalize(vmin=emin, vmax=emax)
         cmap_e = mp.cm.ScalarMappable(norm=norm, cmap=cmap_e)
 
@@ -99,16 +103,17 @@ class BondHoppings(Plot):
             for ib in edges:
                 x1, y1 = H.geometry.xyz[ib, 0], H.geometry.xyz[ib, 1]
                 t = H[ia, ib, 0]
-                self.axes.plot([x0, x1], [y0, y1], '-', color=cmap_t.to_rgba(abs(t)), linewidth=2*abs(t)/tmax, label='%.3f'%t)
+                self.axes.plot([x0, x1], [y0, y1], '-', color=cmap_t.to_rgba(abs(t)), linewidth=2 * abs(t) / tmax, label='%.3f' % t)
                 if annotate:
                     rij = H.geometry.Rij(ia, ib)
-                    self.axes.annotate('%.2f'%(t), (x0+rij[0]*.5, y0+rij[1]*.5), fontsize=8)
+                    self.axes.annotate('%.2f' % t, (x0 + rij[0] / 2, y0 + rij[1] / 2), fontsize=8)
             # Plot onsite energies?
             if not off_diagonal_only:
                 e = H[ia, ia, 0]
-                self.axes.plot(x0, y0, 'o', color=cmap_e.to_rgba(abs(e)), zorder=100, label='%.3f'%e)
+                self.axes.plot(x0, y0, 'o', color=cmap_e.to_rgba(abs(e)), zorder=100, label='%.3f' % e)
 
         self.axes.axis('off')
+        self.axes.set_aspect('equal')
 
 
 class Bonds(Plot):
@@ -129,9 +134,6 @@ class Bonds(Plot):
 
         super().__init__(**kwargs)
 
-        self.plot_bonds(G, annotate=annotate, R=R, **kwargs)
-
-    def plot_bonds(self, G, annotate=False, R=0, **kwargs):
         # Define default kwarg aruments
         cmap = kwargs.get("cmap", plt.cm.jet)
         zorder = kwargs.get("zorder", 1)
@@ -159,18 +161,21 @@ class Bonds(Plot):
         cmap.set_array([])
         for i in G:
             x0, y0 = G.xyz[i, 0], G.xyz[i, 1]
-            idx = G.close(i, R=[0.1, maxR+0.1])
+            idx = G.close(i, R=[0.1, maxR + 0.1])
             for j in idx[1]:
                 x1, y1 = G.xyz[j, 0], G.xyz[j, 1]
                 rij = G.Rij(i, j)
-                d = np.sqrt((rij*rij).sum())
+                d = np.sqrt((rij * rij).sum())
                 if d <= maxR:
                     self.axes.plot([x0, x1], [y0, y1], linewidth=linewidth, color=cmap.to_rgba(d), zorder=zorder, alpha=alpha)
                     if annotate:
-                        self.axes.annotate('%.2f'%(d), (x0+rij[0]*.5, y0+rij[1]*.5), fontsize=8)
+                        self.axes.annotate('%.2f' % d, (x0 + rij[0] / 2, y0 + rij[1] / 2), fontsize=8)
          # Colorbar
         if 'colorbar' in kwargs:
             if kwargs['colorbar'] != False:
                 self.cbar = self.fig.colorbar(cmap)
                 if 'label' in kwargs:
                     self.cbar.ax.set_ylabel(kwargs['label'])
+
+        self.axes.axis('off')
+        self.axes.set_aspect('equal')
