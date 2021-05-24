e0c9f3272c826e9c01da5ecae357d10933ab4e28
Sofia Sanz Wuhl <sofiasw2@gmail.com>
2021-05-24 13:55:14 +0200

    info: added __git_revision__ and __version__ atributes

--- hubbard/__init__.py
+++ hubbard/__init__.py
@@ -44,4 +44,15 @@ from .ncsile import *
 from .density import *
 from .negf import *
 
+from . import info
+from .info import (
+    git_revision as __git_revision__,
+    version as __version__,
+    major as __major__,
+    minor as __minor__,
+    micro as __micro__,
+)
+
 __all__ = [s for s in dir() if not s.startswith('_')]
+__all__ += [f'__{s}__' for s in ['version', 'major', 'minor', 'micro']]
+__all__ += [f'__{s}__' for s in ['git_revision']]
