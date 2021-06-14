07587ffb21baa6db4bc9a1679b653a8a83982dec
Sofia Sanz Wuhl <sofiasw2@gmail.com>
2021-06-14 17:13:57 +0200

    tests: avoid using random density

--- tests/test-hubbard-open.py
+++ tests/test-hubbard-open.py
@@ -17,8 +17,8 @@ H_elec = sp2(ZGNR, t1=2.7, t2=0.2, t3=0.18)
 # Hubbard Hamiltonian of elecs
 MFH_elec = HubbardHamiltonian(H_elec, U=U, nkpt=[102, 1, 1], kT=kT)
 
-# Start with random densities
-MFH_elec.random_density()
+# Initialize spin densities
+MFH_elec.set_polarization([0], dn=[-1]) # Ensure we break symmetry
 # Converge Electrode Hamiltonians
 dn = MFH_elec.converge(density.calc_n, mixer=sisl.mixing.PulayMixer(weight=.7, history=7), tol=1e-10)
 
