ebfe533bbfb96e6f9ff932a2f18dd059cd98e643
Aleksander Bach Lorentzen <abalo@dtu.dk>
2022-05-09 16:03:37 +0200

    test: added BTD test

--- tests/test-transmission-open.py
+++ tests/test-transmission-open.py
@@ -3,16 +3,32 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from hubbard import HubbardHamiltonian, density, NEGF, sp2, plot
+from scipy import sparse as sp
+import time
 
 # Set U for the whole calculation
 U = 3.0
 kT = 0.025
+
+class dummy_class:
+    def __init__(self, pivot, btd):
+        self._piv  = pivot
+        self._btd  = btd
+        lpivot = list(pivot)
+        self._ipiv = np.array([lpivot.index(i) for i in range(len(pivot))])
+    def pivot(self):  return self._piv
+    def ipivot(self): return self._ipiv
+    def btd(self):    return self._btd
+
+
+
 
 # Build zigzag GNR
 ZGNR = sisl.geom.zgnr(2)
 
 # and 3NN TB Hamiltonian
 H_elec = sp2(ZGNR, t1=2.7, t2=0.2, t3=0.18)
+
 
 # Hubbard Hamiltonian of elecs
 MFH_elec = HubbardHamiltonian(H_elec, U=U, nkpt=[102, 1, 1], kT=kT)
@@ -29,21 +45,36 @@ MFH_elec.H.shift(-Ef_elec)
 MFH_elec.H.write('MFH_elec.nc')
 
 # Central region is a repetition of the electrodes without PBC
-HC = H_elec.tile(3, axis=0)
+Ntile = 20
+HC = H_elec.tile(Ntile, axis=0)
 HC.set_nsc([1, 1, 1])
+
+no = HC.geometry.no
+piv = np.arange(no)
+btd = np.array([8] * (Ntile//2))
+tbt  =dummy_class(piv, btd)
+Ov  = sp.identity(no)
 
 # Map electrodes in the device region
 elec_indx = [range(len(H_elec)), range(-len(H_elec), 0)]
 
 # MFH object
-MFH_HC = HubbardHamiltonian(HC.H, n=np.tile(MFH_elec.n, 3), U=U, kT=kT)
+MFH_HC = HubbardHamiltonian(HC.H, n=np.tile(MFH_elec.n, Ntile), U=U, kT=kT)
+
 
 # First create NEGF object
-negf = NEGF(MFH_HC, [(MFH_elec, '-A'), (MFH_elec, '+A')], elec_indx)
+negf = NEGF(MFH_HC, [(MFH_elec, '-A'), (MFH_elec, '+A')], elec_indx, 
+            tbt = tbt, Ov = Ov
+            )
+
+time_start = time.time()
+
 # Converge using Green's function method to obtain the densities
 dn = MFH_HC.converge(negf.calc_n_open, steps=1, mixer=sisl.mixing.PulayMixer(weight=.1), tol=0.1)
 dn = MFH_HC.converge(negf.calc_n_open, steps=1, mixer=sisl.mixing.PulayMixer(weight=1., history=7), tol=1e-6, print_info=True)
 print('Nup, Ndn: ', MFH_HC.n.sum(axis=1))
+
+print("time:", time.time()-time_start)
 
 # Shift device with its Fermi level and write nc file
 MFH_HC.H.write('MFH_HC.nc', Ef=negf.Ef)
