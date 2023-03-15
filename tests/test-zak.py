422f170ff615ddc5b2461f94efd2cadacfeb3271
Thomas Frederiksen <thomas_frederiksen@ehu.eus>
2023-03-15 09:26:48 +0100

    update for sisl change sc-to-lattice

--- tests/test-zak.py
+++ tests/test-zak.py
@@ -10,7 +10,7 @@ for w in range(1, 25, 2):
     print(f'width={w:3}, zak={zak:7.3f}')
 
 # SSH model, topological cell
-g = sisl.Geometry([[0, 0, 0], [0, 1.65, 0]], sisl.Atom(6, 1.001), sc=[10, 3, 10])
+g = sisl.Geometry([[0, 0, 0], [0, 1.65, 0]], sisl.Atom(6, 1.001), lattice=[10, 3, 10])
 g.set_nsc([1, 3, 1])
 H0 = sp2(g)
 H = HubbardHamiltonian(H0, U=0)
@@ -18,7 +18,7 @@ zak = H.get_Zak_phase(axis=1)
 print(f'SSH topo : zak={zak:7.3f}')
 
 # SSH model, trivial cell
-g = sisl.Geometry([[0, 0, 0], [0, 1.42, 0]], sisl.Atom(6, 1.001), sc=[10, 3, 10])
+g = sisl.Geometry([[0, 0, 0], [0, 1.42, 0]], sisl.Atom(6, 1.001), lattice=[10, 3, 10])
 g.set_nsc([1, 3, 1])
 H0 = sp2(g)
 H = HubbardHamiltonian(H0, U=0)
