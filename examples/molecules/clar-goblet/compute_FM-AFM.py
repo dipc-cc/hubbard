9780d1fc057bc5cdce5c368b3821e1db14a4fcef
Sofia Sanz Wuhl <sofiasw2@gmail.com>
2024-01-15 13:03:20 +0100

    fix: update example of clar-goblet

--- examples/molecules/clar-goblet/compute_FM-AFM.py
+++ examples/molecules/clar-goblet/compute_FM-AFM.py
@@ -11,9 +11,6 @@ mol = mol.move(-mol.center(what='xyz'))
 # 3NN tight-binding model
 Hsp2 = sp2(mol, t1=2.7, t2=0.2, t3=.18)
 H = HubbardHamiltonian(Hsp2)
-H.random_density()
-H.set_polarization(up=[6], dn=[28])
-n_AFM = H.n
 
 f = open('FM-AFM.dat', 'w')
 
@@ -22,14 +19,12 @@ for u in np.arange(5, 0, -0.25):
     H.U = u
 
     # AFM case first
-    success = H.read_density('clar-goblet.nc') # Try reading, if we already have density on file
-    if not success:
-        H.n = n_AFM.copy()
+    H.set_polarization(up=[6], dn=[28])
+    H.read_density('clar-goblet.nc', group='AFM') # Try reading, if we already have density on file
 
     dn = H.converge(density.calc_n_insulator, tol=1e-10, mixer=sisl.mixing.PulayMixer(0.7, history=7))
     eAFM = H.Etot
-    H.write_density('clar-goblet.nc')
-    n_AFM = H.n.copy()
+    H.write_density('clar-goblet.nc', group='AFM')
 
     if u == 3.5:
         p = plot.SpinPolarization(H, colorbar=True, vmax=0.4, vmin=-0.4)
@@ -38,14 +33,15 @@ for u in np.arange(5, 0, -0.25):
     # Now FM case
     H.q[0] += 1 # change to two more up-electrons than down
     H.q[1] -= 1
-    success = H.read_density('clar-goblet.nc') # Try reading, if we already have density on file
-    if not success:
-        H.random_density()
+
+    try:
+        H.read_density('clar-goblet.nc', group='FM') # Try reading, if we already have density on file
+    except:
         H.set_polarization(up=[6, 28])
 
     dn = H.converge(density.calc_n_insulator, tol=1e-10, mixer=sisl.mixing.PulayMixer(0.7, history=7))
     eFM = H.Etot
-    H.write_density('clar-goblet.nc')
+    H.write_density('clar-goblet.nc', group='FM')
 
     if u == 3.5:
         p = plot.SpinPolarization(H, colorbar=True, vmax=0.4, vmin=-0.4)
